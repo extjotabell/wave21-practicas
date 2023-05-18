447b0f85eb73e4740988b7d7edc8aaef90df010e
Gaston Leonel Barro <gbarro@ar0c02gf238ml85.ml.com>
2023-05-18 15:46:39 -0300

    Subida Primer Branch Gbarro

---
+++ 2. java/Practica1/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String ciudades[] = new String[10];
+
+        ciudades[0] = "Londres";
+        ciudades[1] = "Madrid";
+        ciudades[3] = "Nueva York";
+        ciudades[4] = "Buenos Aires";
+        ciudades[5] = "Asunción";
+        ciudades[6] = "Sao Pablo";
+        ciudades[7] = "Lima";
+        ciudades[8] = "Lisboa";
+        ciudades[9] = "Tokio";
+
+        short [][] temperaturas = {
+                { -2, 33 },
+                { -3, 32 },
+                { -8, 27 },
+                { 4, 37 },
+                { 6, 42 },
+                { 5, 43 },
+                { 0, 39 },
+                { -7, 26 },
+                { -1, 32 },
+                { -10, 35 }
+        };
+
+        for (int i=0;i<= temperaturas.length;i++){
+
+        }
+
+
+        //System.out.println("Hello world!");
+    }
+}
