06dcae80a81157199764ef550449940bae4839c0
Camila Molina <camila.neculnir@mercadolibre.cl>
2023-05-18 15:11:59 -0400

    cambiar comentarios

--- 2. java/Ejercicio1-vivo1/src/main/java/EjercicioCiudades.java
+++ 2. java/Ejercicio1-vivo1/src/main/java/EjercicioCiudades.java
@@ -3,11 +3,11 @@ public class EjercicioCiudades {
         String[] ciudades = {"Londres", "Madrid", "Nueva York", "Buenos Aires", "Asunción", "Sap Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
         int[][] temperatura = {{-2, 33}, {-3, 32}, {-8, 27}, {4, 37}, {6, 42}, {5, 43}, {0, 39}, {-7, 26}, {-1, 31}, {-10, 35}};
 
-        int menorTemperatura = temperatura[0][0]; // Inicializamos la menor temperatura con el primer valor de Londres
-        String ciudadMenorTemperatura = ciudades[0]; // Inicializamos la ciudad de la menor temperatura con la primera ciudad
+        int menorTemperatura = temperatura[0][0]; // se inicia la menor temperatura con el primer valor de Londres
+        String ciudadMenorTemperatura = ciudades[0]; // se inicia la ciudad de la menor temperatura con la primera ciudad
 
-        int mayorTemperatura = temperatura[0][0]; // Inicializamos la mayor temperatura con el primer valor de Londres
-        String ciudadMayorTemperatura = ciudades[0]; // Inicializamos la ciudad de la mayor temperatura con la primera ciudad
+        int mayorTemperatura = temperatura[0][0]; // aqui se inicia la mayor temperatura con el primer valor de Londres
+        String ciudadMayorTemperatura = ciudades[0]; // se iniciia la ciudad de la mayor temperatura con la primera ciudad
 
         for (int i = 0; i < temperatura.length; i++) {
             if (temperatura[i][0] < menorTemperatura) {
