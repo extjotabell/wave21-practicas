663056a0cf55f28aaf4cefc31ecd6ee304de392f
mtato-meli <matias.tato@mercadolibre.com>
2023-05-19 17:14:03 -0300

    Ejercicio 4 Java

---
+++ 2. java/Java-4/src/PracticaExcepciones.java
@@ -0,0 +1,14 @@
+public class PracticaExcepciones {
+    private int a = 0;
+    private int b = 300;
+
+    public void calculate(){
+        try{
+            int c = b/a;
+        }catch (ArithmeticException e){
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        }finally {
+            System.out.println("Programa finalizado");
+        }
+    }
+}
