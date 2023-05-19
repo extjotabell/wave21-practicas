05661069a3fbb2bd028fa6ff470fcdee8611bff1
Fabrizzio <fabrizzio.camera@mercadolibre.com>
2023-05-19 19:05:33 -0300

    Implementado lo pedido por ejericio practico POO P1

---
+++ 2. java/primer_ejercicio_oop/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona personaVacia = new Persona();
+        Persona personaCompleta = new Persona("Juan Perez", 25,"123456", 56.5, 1.8);
+        Persona personaAMedias = new Persona("Carlos Garcia", 30, "345678");
+
+        //Persona personaInvalida = new Persona("Ana Perez", 29);
+        // no es valido porque no existe constructor con dichos parametros
+
+        System.out.println(personaCompleta.toString());
+        if (personaCompleta.esMayorDeEdad()){
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+        System.out.print("Segun su IMC tiene: ");
+        switch (personaCompleta.calcularIMC()) {
+            case -1 -> System.out.println("Bajo peso");
+            case 0 -> System.out.println("Peso saludable");
+            case 1 -> System.out.println("Sobrepeso");
+        }
+    }
+}
