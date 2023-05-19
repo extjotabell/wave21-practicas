c6af79195867d3e11157c16f23c1526e72e32beb
Jojoseju <jose.duronaraujo@mercadolibre.com.mx>
2023-05-19 09:35:52 -0600

    Agregando ejercicio de java parte 2 clase vivo

---
+++ 2. java/java-p2-vivo/src/Categoria.java
@@ -0,0 +1,36 @@
+public class Categoria {
+
+    private String nombre; // chico, medio, avanzado
+    private String descripcion;
+    private Integer id;
+
+    public Categoria(Integer id, String nombre, String descripcion ) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
