c912736c56a092b2356565e97dab2520e6fdfe4d
ornellaquarracino <ornella.quarracino@mercadolibre.com>
2023-05-22 09:31:33 -0300

    Agregando ejercitación del vivo de POO P2

---
+++ 2. java/Vivo_POO_P2_Distribuidora/src/main/java/org/example/Distribuidora.java
@@ -0,0 +1,45 @@
+package org.example;
+import java.util.ArrayList;
+
+/*Crear una clase ejecutable llamada Distribuidora donde van a crear un array de productos,
+imprimir el precio total al vender 5 productos de cada tipo.
+Crear los elementos del array con los datos que quieras.*/
+
+public class Distribuidora {
+    public static void main(String[] args) {
+        ArrayList<Producto> productos = new ArrayList();
+
+        // No Perecederos
+        Producto p1 = new NoPerecedero(20, "Auriculares", "Electronica");
+        productos.add(p1);
+        Producto p2 = new NoPerecedero(30, "teclado", "Electronica");
+        productos.add(p2);
+        Producto p3 = new NoPerecedero(45, "Mousepad", "Electronica");
+        productos.add(p3);
+        Producto p4 = new NoPerecedero(160, "macPro", "Electronica");
+        productos.add(p4);
+        Producto p5 = new NoPerecedero(130, "mouse", "Electronica");
+        productos.add(p5);
+
+
+        // Perecederos
+        Producto p6 = new Perecedero(300, "queso", 2);
+        productos.add(p6);
+        Producto p7 = new Perecedero(300, "jamon", 1);
+        productos.add(p7);
+        Producto p8 = new Perecedero(280, "papas", 4);
+        productos.add(p8);
+        Producto p9 = new Perecedero(290, "yogur", 3);
+        productos.add(p9);
+        Producto p10 = new Perecedero(90, "Mani", 3);
+        productos.add(p10);
+
+        double precioTotal = 0;
+        for (Producto p: productos) {
+            precioTotal = p.calcular((int)Math.floor((Math.random() * 100) + 1));
+        }
+
+        System.out.println("El Precio total es de: " + precioTotal);
+    }
+}
+
---
+++ 2. java/Vivo_POO_P2_Distribuidora/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Crear una clase PracticaExcepciones que defina los atributos a = 0 y b = 300
+         de tipo int. Calcular el cociente de b/a. Controlar la excepción que se lanza
+         indicando el mensaje “Se ha producido un error”. Al final del programa
+         siempre deberá indicar el mensaje “Programa finalizado”
+         */
+
+        // Ejercicio 1
+        PracticaExcepciones p = new PracticaExcepciones(0, 300);
+        double result = p.calcularCociente1();
+        if(result != -1){
+            System.out.println(String.format("El Resultado de la division es: %f", result));
+        }
+
+        // Ejercicio 2
+        p = new PracticaExcepciones(0, 300);
+        result = p.calcularCociente2();
+        if(result != -1){
+            System.out.println(String.format("El Resultado de la division es: %f", result));
+        }
+    }
+}
