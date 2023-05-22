fd34d2ee87094512b6fa53df395897c94de6b9da
MartinBistolfi <martin.bistolfi@mercadolibre.com>
2023-05-22 11:36:38 -0300

    Subida ej 2 poo

--- 2. java/introduccionAPOOej2/src/Distribuidora.java
+++ 2. java/introduccionAPOOej2/src/Distribuidora.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Distribuidora {
     public static void main(String[] args) {
+        List<Producto> productos = new ArrayList<Producto>();
+        productos.add(new Perecedero("Atun", 50, 2));
+        productos.add(new Perecedero("Arroz", 80, 1));
+        productos.add(new Perecedero("Azucar", 60, 3));
+        productos.add(new NoPerecedero("Pollo", 150, "Carne"));
+        productos.add(new NoPerecedero("Queso", 100, "Lacteos"));
+
+
+        for(int i = 0; i < productos.size(); i++){
+            System.out.println("Precio total del " + productos.get(i).nombre + ": " +productos.get(i).calcular(5));
+        }
+
     }
 }
         
