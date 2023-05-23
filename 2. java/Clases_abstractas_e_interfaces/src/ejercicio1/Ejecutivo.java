56eb427352c9c7771a2806d1d4d705db0d32897f
hehernandezMELI <helmer.hernandez@mercadolibre.com.co>
2023-05-23 12:09:53 -0500

    Ejercicios VIVO clases abstractas e interfaces

---
+++ 2. java/Clases_abstractas_e_interfaces/src/ejercicio1/Ejecutivo.java
@@ -0,0 +1,12 @@
+package ejercicio1;
+
+public class Ejecutivo {
+    public void deposito(){
+        System.out.println("Deposito realizado con éxito");
+    }
+
+    public void transferencia(){
+        System.out.println("Transferencia realizada con éxito");
+    }
+
+}
---
+++ 2. java/Clases_abstractas_e_interfaces/src/ejercicio3/Perro.java
@@ -0,0 +1,13 @@
+package ejercicio3;
+
+public class Perro extends Animal implements Carnivoro{
+    @Override
+    public void emitirSonido() {
+        System.out.println("guau");
+    }
+
+    @Override
+    public void comerCarne() {
+        System.out.println("Como carne");
+    }
+}
