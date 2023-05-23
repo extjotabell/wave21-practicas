6827e94b2a1ae920a0aca97757cf4bac28434c12
laurasanabriameli <laura.sanabria@mercadolibre.com.co>
2023-05-23 12:12:38 -0500

    Agrega ejercicio de clases abstractas e interfaces

---
+++ 2. java/Clases_abstractas_e_interfaces/src/ejercicio1/Transaccion.java
@@ -0,0 +1,8 @@
+package ejercicio1;
+
+public interface Transaccion {
+
+    public void transaccionOk();
+    public void transaccionNoOk();
+
+}
---
+++ 2. java/Clases_abstractas_e_interfaces/src/ejercicio3/Herbivoro.java
@@ -0,0 +1,6 @@
+package ejercicio3;
+
+public interface Herbivoro {
+    void comerHierba();
+
+}
