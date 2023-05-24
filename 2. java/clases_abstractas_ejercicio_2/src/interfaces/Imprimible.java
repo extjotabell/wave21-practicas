b74e3e7f962bed7deb6968b0df53eefcb36537de
gantonietti <gabriel.antonietti@mercadolibre.com>
2023-05-24 10:17:48 -0300

    ejercicio 2 clase abstracta-imprimir documentos

---
+++ 2. java/clases_abstractas_ejercicio_2/src/interfaces/Imprimible.java
@@ -0,0 +1,13 @@
+package src.interfaces;
+
+import src.clases.Documento;
+
+public interface Imprimible {
+
+    static void imprimirDocumento(Documento documento){
+
+        documento.imprimir();
+    };
+
+    void imprimirTipoDoc();
+}
