8b23a75b186f1598a37e9728a58516783faa1e28
Mercedes Magnelli <mercedes.magnelli@mercadolibre.com>
2023-05-24 11:10:42 -0300

    subo ejercicio de lambdas del 23/5

---
+++ 2. java/Lambdas/src/main/java/org/example/Main.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Comparator<Vehiculo> precio = Comparator.comparing(Vehiculo::getPrecio);
+    Comparator<Vehiculo> marca = Comparator.comparing(Vehiculo::getMarca);
+    Comparator<Vehiculo> marcaPrecio = marca.thenComparing(precio);
+
+    List<Vehiculo> vehiculos = Arrays.asList(
+      new Vehiculo("Fiesta", "Ford", 1000d),
+      new Vehiculo("Kangoo", "Ford", 1200d),
+      new Vehiculo("Explorer", "Ford", 2500d),
+      new Vehiculo("Uno", "Fiat", 500d),
+      new Vehiculo("Cronos", "Fiat", 1000d),
+      new Vehiculo("Torino", "Fiat", 1250d),
+      new Vehiculo("Aveo", "Chevrolet", 1250d),
+      new Vehiculo("Spin", "Chevrolet", 2500d),
+      new Vehiculo("Fortuner", "Toyota", 1200d),
+      new Vehiculo ("Corola", "Toyota", 3000d),
+      new Vehiculo("Renault", "Logan", 950d)
+    );
+
+    Garaje garaje = new Garaje(1,vehiculos);
+
+    garaje.getVehiculos()
+      .stream()
+      .sorted(precio)
+      .map(Vehiculo::toString)
+      .forEach(System.out::println);
+    System.out.println("----------");
+
+    garaje.getVehiculos()
+      .stream()
+      .sorted(marcaPrecio)
+      .map(Vehiculo::toString)
+      .forEach(System.out::println);
+
+  }
+}
