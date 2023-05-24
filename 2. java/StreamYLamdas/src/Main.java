b34a245016be86a30491c04eaed806aa187c7a65
gbarro <gaston.barro@mercadolibre.com>
2023-05-24 11:14:35 -0300

    Ejercicios de practica de Stream y Lambdas avanzado (falta arreglar ejercicio 4)

--- 2. java/StreamYLamdas/src/Main.java
+++ 2. java/StreamYLamdas/src/Main.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 
+        //Generamos una lista de vehiculos y completamos con los datos
         List<Vehiculo> vehiculosList = new ArrayList<>();
 
         vehiculosList.add(new Vehiculo("Ford", "Fiesta", 1000));
@@ -20,14 +23,42 @@ public class Main {
         vehiculosList.add(new Vehiculo("Toyota", "Fortuner", 3000));
         vehiculosList.add(new Vehiculo("Renault", "Logan", 950));
 
+
+        // Creamos el objeto Garage y le pasamos la lista de los vehiculos
         Garage GarageCorrientes = new Garage(120, vehiculosList);
 
-        //Ejercicio3: Imprimir ordenando por Precio de menor a mayor
 
+        //Ejercicio3: Imprimir ordenamos por Precio de menor a mayor
+        System.out.println("Ejercicio3: Imprimir ordenando por costo de menor a mayor");
+        System.out.println("---------------------------------------------------------");
         GarageCorrientes.getVehiculos()
                 .stream()
-                .sorted((x,y) -> x.getCosto().compareTo(y.getCosto()))
+                .sorted((x, y) -> x.getCosto().compareTo(y.getCosto()))
                 .forEach(vehiculo -> System.out.println(vehiculo));
+        System.out.println("---------------------------------------------------------"+"\n");
 
+        //Ejercicio4: Imprimir ordenando por marca y luego el costo de menor a mayor
+        System.out.println("Ejercicio4: Imprimir ordenando por marca y luego el costo de menor a mayor");
+        System.out.println("--------------------------------------------------------------------------");
+        GarageCorrientes.getVehiculos()
+                .stream()
+                .sorted((x, y) -> x.getMarca().compareTo(y.getMarca()))
+                .sorted((i, j) -> i.getCosto().compareTo(j.getCosto()))
+                .forEach(vehiculo -> System.out.println(vehiculo));
+        System.out.println("--------------------------------------------------------------------------"+"\n");
+
+
+        //Ejercicio 5: Extraer los autos con precio menor a 1000 y luego sacar un promedio de los mismos
+        System.out.println("Extraer los autos con precio menor a 1000 y luego sacar un promedio de los mismos");
+        System.out.println("---------------------------------------------------------------------------------");
+        OptionalDouble promedio =
+        GarageCorrientes.getVehiculos()
+                .stream()
+                .filter(vehiculo -> vehiculo.getCosto() <=1000)
+                .mapToInt(Vehiculo::getCosto).average();
+        System.out.println("El Valor promedio de los vehiculos con costo menor a 1000 es: " + promedio);
+        System.out.println("--------------------------------------------------------------------------------"+"\n");
+
+
+        }
     }
-}
