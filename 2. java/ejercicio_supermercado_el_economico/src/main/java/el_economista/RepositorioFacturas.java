1bd2bdffaad0f7591a7daa9ee4fdee412ed38062
Fabrizzio <fabrizzio.camera@mercadolibre.com>
2023-05-26 17:09:36 -0300

    implementado repositorio facturas, que implementa CRUD

---
+++ 2. java/ejercicio_supermercado_el_economico/src/main/java/el_economista/RepositorioFacturas.java
@@ -0,0 +1,45 @@
+package el_economista;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RepositorioFacturas implements CRUD<Factura>{
+
+    private List<Factura> facturas;
+    private int nextId;
+
+    public RepositorioFacturas() {
+        this.facturas = new ArrayList<>();
+        this.nextId = 1;
+    }
+
+    @Override
+    public Factura getElemento(int id) {
+        return facturas.stream().filter(c -> c.getId() == id).findAny().orElse(null);
+    }
+
+    @Override
+    public List<Factura> getElementos() {
+        return Collections.unmodifiableList(this.facturas);
+    }
+
+    @Override
+    public void agregarElemento(Factura elem) {
+        elem.setId(this.nextId++);
+        this.facturas.add(elem);
+    }
+
+    @Override
+    public Factura modificarElemento(Factura nuevoElem) {
+        Factura facturaVieja = this.getElemento(nuevoElem.getId());
+        int indexOfFactura = this.facturas.indexOf(facturaVieja);
+        this.facturas.set(indexOfFactura, nuevoElem);
+        return this.facturas.get(indexOfFactura);
+    }
+
+    @Override
+    public void removerElemento(Factura elem) {
+        this.facturas.remove(elem);
+    }
+}
