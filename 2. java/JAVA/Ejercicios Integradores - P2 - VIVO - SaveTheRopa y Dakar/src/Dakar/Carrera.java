c88bbb4d23eca113a377107529c8ba41d3522441
Javier Rydel <javier.rydel@mercadolibre.com>
2023-05-26 16:52:29 -0300

    Se agregan partes 6 y 7 del Dakar

--- 2. java/JAVA/Ejercicios Integradores - P2 - VIVO - SaveTheRopa y Dakar/src/Dakar/Carrera.java
+++ 2. java/JAVA/Ejercicios Integradores - P2 - VIVO - SaveTheRopa y Dakar/src/Dakar/Carrera.java
@@ -10,12 +10,17 @@ public class Carrera {
     private Integer cantidadDeVehiculosPermitidos;
     private List<Vehiculo> listaDeVehiculos;
 
+    private SocorristaAuto socorristaAuto;
+    private SocorristaMoto socorristaMoto;
+
     public Carrera(Integer distancia, Integer premioEnDolares, String nombre, Integer cantidadDeVehiculosPermitidos) {
         this.distancia = distancia;
         this.premioEnDolares = premioEnDolares;
         this.nombre = nombre;
         this.cantidadDeVehiculosPermitidos = cantidadDeVehiculosPermitidos;
         this.listaDeVehiculos = new ArrayList<Vehiculo>();
+        this.socorristaAuto = new SocorristaAuto();
+        this.socorristaMoto = new SocorristaMoto();
     }
 
     public void darDeAltaAuto(Integer velocidad, Integer aceleracion, Double anguloDeGiro, Integer patente) {
@@ -56,6 +61,52 @@ public class Carrera {
                 break;
             }else{
                 System.out.println("Ese vehiculo no esta en el sistema!");            }
+        }
+    }
+
+    public Double formulaGanadora(Vehiculo v){
+        return (v.getVelocidad() * (v.getAceleracion() / 2) ) / (v.getAnguloDeGiro() * (v.getPeso() - v.getRuedas() * 100));
+    }
+    public void ganador(){
+        Double max = 0.0;
+        Integer patenteDelGanador = 0;
+        for (Vehiculo v : listaDeVehiculos){
+            Double valorActual = formulaGanadora(v);
+            if(valorActual > max){
+                max = valorActual;
+                patenteDelGanador = v.getPatente();
+            }
+        }
+        System.out.println("La patente del vehiculo gabador es " + patenteDelGanador + "! Con un resultado de " + max);
+    }
+
+    public void socorrerAuto(String patente){
+        Auto autoASocorrer = null;
+        for(Vehiculo v : listaDeVehiculos){
+            if(v.getPatente().toString().equals(patente) && v instanceof Auto){
+                autoASocorrer = (Auto) v;       //Al ser v del tipo Vehiculo, lo casteo a Auto
+            }
+        }
+
+        if(autoASocorrer != null){
+            socorristaAuto.socorrer(autoASocorrer);
+        }else {
+            System.out.println("No existe un Auto con esa patente!");
+        }
+    }
+
+    public void socorrerMoto(String patente){
+        Moto motoASocorrer = null;
+        for(Vehiculo v : listaDeVehiculos){
+            if(v.getPatente().toString().equals(patente) && v instanceof Moto){
+                motoASocorrer = (Moto) v;       //Al ser v del tipo Vehiculo, lo casteo a Auto
+            }
+        }
+
+        if(motoASocorrer != null){
+            socorristaMoto.socorrer(motoASocorrer);
+        }else {
+            System.out.println("No existe una Moto con esa patente!");
         }
     }
 
--- 2. java/JAVA/Ejercicios Integradores - P2 - VIVO - SaveTheRopa y Dakar/src/Dakar/Main.java
+++ 2. java/JAVA/Ejercicios Integradores - P2 - VIVO - SaveTheRopa y Dakar/src/Dakar/Main.java
@@ -7,11 +7,12 @@ public class Main {
 
         carrera.darDeAltaAuto(200, 10, 10.0, 1234);
         carrera.darDeAltaMoto(150, 30, 50.0, 5678);
-        carrera.darDeAltaMoto(150, 30, 50.0, 5679);
+        carrera.darDeAltaMoto(140, 25, 50.0, 9101);
 
         Vehiculo vehiculoAEliminar = carrera.getListaDeVehiculos().get(1);
 
-        carrera.eliminarVehiculo(vehiculoAEliminar);
+        carrera.ganador();
 
+        carrera.socorrerMoto("5678");
     }
 }
