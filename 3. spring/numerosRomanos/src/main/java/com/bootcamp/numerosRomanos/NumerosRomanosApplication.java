5ec639b83d625c58e5a486116e61ae63482f7dbf
Fabrizzio <fabrizzio.camera@mercadolibre.com>
2023-05-30 13:59:25 -0300

    terminado ejercicio de numeros romanos

--- 3. spring/numerosRomanos/src/main/java/com/bootcamp/numerosRomanos/NumerosRomanosApplication.java
+++ 3. spring/numerosRomanos/src/main/java/com/bootcamp/numerosRomanos/NumerosRomanosApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NumerosRomanosApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(NumerosRomanosApplication.class, args);
 	}
 
