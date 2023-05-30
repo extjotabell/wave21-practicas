c3513f0d9bafe465c50ad685b885475bab993fba
ornellaquarracino <ornella.quarracino@mercadolibre.com>
2023-05-30 18:55:00 -0300

    subiendo ejercicio terminado de Springboot de Numeros Romanos

---
+++ 3. spring/numerosRomanos/src/main/java/com/numRomanos/numerosRomanos/controller/Controller.java
@@ -0,0 +1,21 @@
+package com.numRomanos.numerosRomanos.controller;
+
+import com.numRomanos.numerosRomanos.service.Convertidor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    Convertidor convertidor;
+
+    // PathVariable se agrega para usar variables en la url
+    @GetMapping("/{numero}")
+    public String convertirARomano(@PathVariable String numero){
+        return "El numero romano es: " + convertidor.conversorANumeroRomano(numero);
+    }
+
+}
