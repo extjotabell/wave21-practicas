0e87833de675f0e47b14d6e6fb95e10fb456ee88
Charlie Schiaffino <carlos.schiaffino@mercadolibre.cl>
2023-05-30 14:21:52 -0400

    chore(spring): VIVO - Ejercicio números romanos.

---
+++ 3. spring/demo/src/main/java/com/example/demo/services/Convertidor.java
@@ -0,0 +1,52 @@
+package com.example.demo.services;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class Convertidor {
+
+    private final Map<Integer, Character> romanNumbers;
+
+    public Convertidor() {
+        this.romanNumbers = new HashMap<>();
+        this.romanNumbers.put(1, 'I');
+        this.romanNumbers.put(5, 'V');
+        this.romanNumbers.put(10, 'X');
+        this.romanNumbers.put(50, 'L');
+        this.romanNumbers.put(100, 'C');
+        this.romanNumbers.put(500, 'D');
+        this.romanNumbers.put(1000, 'M');
+    }
+
+    public String conversorANumeroRomano(String numero){
+        int numDigits = numero.length();
+
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < numDigits; ++i) {
+            char src = numero.charAt(i); // orig
+            int number = (src - '0'); // convert to integer
+            int place = (numDigits - 1) - i;
+            int absolute = (int)Math.pow(10, place);
+            if (number == 9) {
+                res.append(romanNumbers.get(absolute));
+                res.append(romanNumbers.get(absolute * 10));
+            }
+            else if (number >= 5) {
+                res.append(romanNumbers.get(absolute * 5));
+                res.append((String.valueOf(romanNumbers.get(absolute)))
+                        .repeat(number - 5));
+            }
+            else if (number == 4) {
+                res.append(romanNumbers.get(absolute));
+                res.append(romanNumbers.get(absolute * 5));
+            }
+            else {
+                res.append((String.valueOf(romanNumbers.get(absolute)))
+                        .repeat(number));
+            }
+        }
+        return res.toString();
+    }
+}
