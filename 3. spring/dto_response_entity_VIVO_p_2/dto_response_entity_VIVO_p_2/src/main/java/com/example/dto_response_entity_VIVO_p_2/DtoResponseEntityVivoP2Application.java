b8c013dd9dbcd4e476289481ed08e27a4c7efca4
Dominguez Gaston Matias <gaston.gdominguez@mercadolibre.com>
2023-05-31 14:28:11 -0300

    add dtp response entity VIVO parte 2

---
+++ 3. spring/dto_response_entity_VIVO_p_2/dto_response_entity_VIVO_p_2/src/main/java/com/example/dto_response_entity_VIVO_p_2/DtoResponseEntityVivoP2Application.java
@@ -0,0 +1,13 @@
+package com.example.dto_response_entity_VIVO_p_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoResponseEntityVivoP2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtoResponseEntityVivoP2Application.class, args);
+	}
+
+}
---
+++ 3. spring/dto_response_entity_VIVO_p_2/dto_response_entity_VIVO_p_2/src/test/java/com/example/dto_response_entity_VIVO_p_2/DtoResponseEntityVivoP2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dto_response_entity_VIVO_p_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoResponseEntityVivoP2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
