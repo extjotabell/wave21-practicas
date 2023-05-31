89f1e229f8e069f3b64c3b0a1abec01f296f8a00
crisbustaq <cristobal.bustamante@mercadolibre.cl>
2023-05-31 14:01:22 -0400

    Se elimina import de clase Deporte en PersonaDTO

--- 3. spring/ejerciciosPracticos/deportistas/src/main/java/com/ejercicio/deportistas/dto/PersonaDTO.java
+++ 3. spring/ejerciciosPracticos/deportistas/src/main/java/com/ejercicio/deportistas/dto/PersonaDTO.java
@@ -1,7 +1,6 @@
 package com.ejercicio.deportistas.dto;
 
 
-import com.ejercicio.deportistas.classes.Deporte;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
