fbce03959648654bf839eb01fb4fd1fce7b2c846
Dominguez Gaston Matias <gaston.gdominguez@mercadolibre.com>
2023-05-31 08:01:03 -0300

    update romanos project

--- 3. spring/spring_P2_PG/spring_P2_PG/src/main/java/com/example/spring_P2_PG/convertir.java
+++ 3. spring/spring_P2_PG/spring_P2_PG/src/main/java/com/example/spring_P2_PG/convertir.java
@@ -4,28 +4,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Array;
 import java.util.Arrays;
-import java.util.Map;
 
 @RestController
 public class convertir {
     @GetMapping("/{numero}")
-    public String convertirNumero(@PathVariable Integer numero) {
+    public StringBuilder convertirNumero(@PathVariable Integer numero) {
 
-        int[] numerosConvertir = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] numerosRomanos = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] numerosConvertir = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 2, 1};
+        String[] numerosRomanos = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "II", "I"};
 
-        String romano = "";
+        StringBuilder romano = new StringBuilder();
 
-        for (int i=0; i<numerosConvertir.length; i++) {
-            if(numero >= numerosConvertir[i]) {
-                romano += numerosRomanos[i];
-                numero -= numerosConvertir[i];
+        if (numero >= 4000) {
+            return new StringBuilder("Numero no valido");
+        } else {
+            for (int i=0; i<numerosConvertir.length; i++) {
+                while (numero >= numerosConvertir[i]) {
+                    numero -= numerosConvertir[i];
+                   romano.append(numerosRomanos[i]);
+                }
             }
+
+            return romano;
+
         }
 
-        return romano;
+
+
     }
 
 
