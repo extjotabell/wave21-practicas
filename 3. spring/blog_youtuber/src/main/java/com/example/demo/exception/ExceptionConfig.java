2557dbb47836c0d4a850c7e2602b0c260e0c78b2
ezesafdie-meli <ezequiel.safdie@mercadolibre.com>
2023-06-05 12:32:07 -0300

    Pruebas con excepciones

--- 3. spring/blog_youtuber/src/main/java/com/example/demo/exception/ExceptionConfig.java
+++ 3. spring/blog_youtuber/src/main/java/com/example/demo/exception/ExceptionConfig.java
@@ -1,16 +1,21 @@
 package com.example.demo.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice(annotations = RestController.class)
-public class ExceptionConfig {
+public class ExceptionConfig extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<?> notFoundException(Exception e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    protected ResponseEntity<Object> notFoundException(Exception e, WebRequest request) {
+        String bodyOfResponse = "EntryBlog not found";
+        return handleExceptionInternal(e, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(AlreadyExistsException.class)
