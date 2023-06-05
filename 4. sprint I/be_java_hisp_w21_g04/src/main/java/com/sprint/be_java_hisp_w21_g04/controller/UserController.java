23922a49a3e60ba61de6e14ae9aaf33730159fdb
laurasanabriameli <laura.sanabria@mercadolibre.com.co>
2023-06-05 17:47:12 -0500

    Agrega funcionalidad de US003 : Obtiene followers de un vendedor por id

--- 4. sprint I/be_java_hisp_w21_g04/src/main/java/com/sprint/be_java_hisp_w21_g04/controller/UserController.java
+++ 4. sprint I/be_java_hisp_w21_g04/src/main/java/com/sprint/be_java_hisp_w21_g04/controller/UserController.java
@@ -1,4 +1,29 @@
 package com.sprint.be_java_hisp_w21_g04.controller;
 
+import com.sprint.be_java_hisp_w21_g04.dto.response.FollowersResponseDto;
+import com.sprint.be_java_hisp_w21_g04.dto.response.UserResponseDto;
+import com.sprint.be_java_hisp_w21_g04.service.user.IUserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
 public class UserController {
+
+    private IUserService _userService;
+
+    public UserController(IUserService userService) {
+        this._userService = userService;
+    }
+
+    @GetMapping("/users/{userId}/followers/list")
+    public ResponseEntity<?> getFollowersById(@PathVariable int userId){
+        FollowersResponseDto followers = _userService.getFollowersById(userId);
+        return new ResponseEntity<>(followers, HttpStatus.OK);
+    }
+
 }
--- 4. sprint I/be_java_hisp_w21_g04/src/main/java/com/sprint/be_java_hisp_w21_g04/service/user/IUserService.java
+++ 4. sprint I/be_java_hisp_w21_g04/src/main/java/com/sprint/be_java_hisp_w21_g04/service/user/IUserService.java
@@ -1,4 +1,13 @@
 package com.sprint.be_java_hisp_w21_g04.service.user;
 
+import com.sprint.be_java_hisp_w21_g04.dto.response.FollowersResponseDto;
+import com.sprint.be_java_hisp_w21_g04.dto.response.UserResponseDto;
+
+import java.util.List;
+
 public interface IUserService {
+
+
+    public FollowersResponseDto getFollowersById(int user_id);
+
 }
