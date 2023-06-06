fef1333dbf31f8d3df9b67f3eb5d4856a533a1f1
ddelavegaML <demian.delavega@mercadolibre.com>
2023-06-06 16:51:47 -0300

    Borrado de comentarios

--- 4. sprint I/be_java_hisp_w21_g1/src/main/java/com/example/be_java_hisp_w21_g1/Controller/Controller.java
+++ 4. sprint I/be_java_hisp_w21_g1/src/main/java/com/example/be_java_hisp_w21_g1/Controller/Controller.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-public class  Controller {
+public class Controller {
     @Autowired
     IUserService userService;
 
@@ -23,7 +23,7 @@ public class  Controller {
     //Recibe FollowPostDTO
     //Retorna status code
     @PostMapping("/users/{userId}/follow/{userIdToFollow}")
-    public ResponseEntity<?> follow(@PathVariable int userId, @PathVariable int userIdToFollow){
+    public ResponseEntity<?> follow(@PathVariable int userId, @PathVariable int userIdToFollow) {
         return null;
     }
 
@@ -31,7 +31,7 @@ public class  Controller {
     //Recibe UserIdDTO
     //Retorna FollowersCountDTO
     @GetMapping("/users/{userId}/followers/count")
-    public ResponseEntity<FollowersCountDTO> countFollowers(@PathVariable int userId){
+    public ResponseEntity<FollowersCountDTO> countFollowers(@PathVariable int userId) {
         return new ResponseEntity<>(userService.getFollowersCount(userId), HttpStatus.OK);
     }
 
@@ -39,7 +39,7 @@ public class  Controller {
     //Recibe UserIdDTO
     //Retorna FollowerListDTO
     @GetMapping("/users/{userId}/followers/list")
-    public ResponseEntity<FollowerListDTO> listFollowers(@PathVariable int userId, @RequestParam(value = "order", required = false) String alf_order){
+    public ResponseEntity<FollowerListDTO> listFollowers(@PathVariable int userId, @RequestParam(value = "order", required = false) String alf_order) {
         return new ResponseEntity<>(userService.getFollowersList(userId, alf_order), HttpStatus.OK);
     }
 
@@ -47,7 +47,7 @@ public class  Controller {
     //Recibe UserIdDTO
     //Retorna FollowedListDTO
     @GetMapping("/users/{userId}/followed/list")
-    public ResponseEntity<FollowedListDTO> listFollowed(@PathVariable int userId, @RequestParam(value = "order", required = false) String alf_order){
+    public ResponseEntity<FollowedListDTO> listFollowed(@PathVariable int userId, @RequestParam(value = "order", required = false) String alf_order) {
         return new ResponseEntity<>(userService.getFollowedList(userId, alf_order), HttpStatus.OK);
     }
 
@@ -55,7 +55,7 @@ public class  Controller {
     //Recibe PostProductDTO
     //Retorna status code
     @PostMapping("/products/post")
-    public ResponseEntity<?> postProduct(@RequestBody PostProductDTO body){
+    public ResponseEntity<?> postProduct(@RequestBody PostProductDTO body) {
         return null;
     }
 
@@ -65,7 +65,7 @@ public class  Controller {
     //Recibe UserIdDTO
     //Retorna PostBySeller
     @GetMapping("/products/followed/{userId}/list")
-    public ResponseEntity<?> latestsPosts(@PathVariable int userId){
+    public ResponseEntity<?> latestsPosts(@PathVariable int userId) {
         return null;
     }
 
@@ -78,28 +78,6 @@ public class  Controller {
         return null;
     }
 
-    //US 0008: Ordenamiento alfabético ascendente y descendente
-//    @GetMapping("/users/{UserId}/followers/list")
-//    public ResponseEntity<?> orderFollowersBy(@PathVariable int userId, @RequestParam(value = "order", required = false) String alf_order){
-//        return null;
-//    }
-
-//    @GetMapping("/users/{UserID}/followed/list?order=name_asc")
-//    public ResponseEntity<?> orderFollowedBy(@PathVariable int userId, @RequestParam(value = "order", required = true) String alf_order){
-//        return null;
-//    }
-
-    //US 0009: Ordenamiento por fecha ascendente y descendente
-    /*
-    * /products/followed/{userId}/list?order=date_asc
-    /products/followed/{userId}/list?order=date_desc
-
-    @GetMapping("products/followed/{userId}/list")
-    public ResponseEntity<?> orderProductsBy(@PathVariable int userId, @RequestParam(value = "order", required = true) String dateOrder){
-        return null;
-    }
-
-    */
     //US 0010: Llevar a cabo la publicación de un nuevo producto en promoción
     @PostMapping("/products/promo-post")
     public ResponseEntity<?> newProductInSale() {
@@ -109,13 +87,13 @@ public class  Controller {
     //US 0011: Obtener la cantidad de productos en promoción de un determinado vendedor
 
     @GetMapping("/products/promo-post/count")
-    public ResponseEntity<?> promoProductsCount(@RequestParam(value = "user_id", required = true) int userId){
+    public ResponseEntity<?> promoProductsCount(@RequestParam(value = "user_id", required = true) int userId) {
         return null;
     }
 
     //US 0012: OPCIONAL
     @GetMapping("/products/promo-post/list?user_id={userId}")
-    public ResponseEntity<?> productsInSaleBySeller(@PathVariable int userId){
+    public ResponseEntity<?> productsInSaleBySeller(@PathVariable int userId) {
         return null;
     }
 
