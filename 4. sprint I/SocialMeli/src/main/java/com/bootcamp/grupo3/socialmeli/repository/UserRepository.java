25201f6e914fd59aca42f79382c7c813a9885ebb
mtato-meli <matias.tato@mercadolibre.com>
2023-06-06 10:45:00 -0300

    ajustes en user repository

--- 4. sprint I/SocialMeli/src/main/java/com/bootcamp/grupo3/socialmeli/repository/UserRepository.java
+++ 4. sprint I/SocialMeli/src/main/java/com/bootcamp/grupo3/socialmeli/repository/UserRepository.java
@@ -13,14 +13,14 @@ public class UserRepository implements IUserRepository {
     private final List<User> users = new ArrayList<>();
 
     public UserRepository(){
-        users.add(User.builder().id(1).name("mtato").followed(new ArrayList<>()).followers(new ArrayList<>()).build());
-        users.add(User.builder().id(2).name("luchoDeMeli").followed(new ArrayList<>()).followers(new ArrayList<>()).build());
-        users.add(User.builder().id(3).name("GabitoDeMerlo").followed(new ArrayList<>()).followers(new ArrayList<>()).build());
-        users.add(User.builder().id(4).name("LaOrneDeFlorida").followed(new ArrayList<>()).followers(new ArrayList<>()).build());
-        users.add(User.builder().id(5).name("Juansito").followed(new ArrayList<>()).followers(new ArrayList<>()).build());
-        users.add(User.builder().id(6).name("nauhel").followed(new ArrayList<>()).followers(new ArrayList<>()).build());
-        users.add(User.builder().id(7).name("Favrisio").followed(new ArrayList<>()).followers(new ArrayList<>()).build());
-        users.add(User.builder().id(8).name("Mersh").followed(new ArrayList<>()).followers(new ArrayList<>()).build());
+        users.add(User.builder().id(1).name("mtato").build());
+        users.add(User.builder().id(2).name("luchoDeMeli").build());
+        users.add(User.builder().id(3).name("GabitoDeMerlo").build());
+        users.add(User.builder().id(4).name("LaOrneDeFlorida").build());
+        users.add(User.builder().id(5).name("Juansito").build());
+        users.add(User.builder().id(6).name("nauhel").build());
+        users.add(User.builder().id(7).name("Favrisio").build());
+        users.add(User.builder().id(8).name("Mersh").build());
     }
 
     @Override
