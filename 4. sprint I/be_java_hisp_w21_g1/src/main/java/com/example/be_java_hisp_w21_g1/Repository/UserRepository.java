9c437aa2eca12c71368ff04730d31d78baa744bc
ddelavegaML <demian.delavega@mercadolibre.com>
2023-06-06 17:04:34 -0300

    Formateado

--- 4. sprint I/be_java_hisp_w21_g1/src/main/java/com/example/be_java_hisp_w21_g1/Repository/UserRepository.java
+++ 4. sprint I/be_java_hisp_w21_g1/src/main/java/com/example/be_java_hisp_w21_g1/Repository/UserRepository.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 
 @Repository
-public class UserRepository implements IUserRepository{
+public class UserRepository implements IUserRepository {
 
     private List<User> users = new ArrayList<>();
 
@@ -28,47 +28,50 @@ public class UserRepository implements IUserRepository{
     }
 
     private void init() {
-try {
-    Product product1 = new Product(1, "Producto1", "Type1", "Brand1", "Color1", "Notes1");
-    Product product2 = new Product(2, "Producto2", "Type2", "Brand2", "Color2", "Notes2");
-    Product product3 = new Product(3, "Producto3", "Type3", "Brand3", "Color3", "Notes3");
+        try {
+            Product product1 = new Product(1, "Producto1", "Type1", "Brand1", "Color1", "Notes1");
+            Product product2 = new Product(2, "Producto2", "Type2", "Brand2", "Color2", "Notes2");
+            Product product3 = new Product(3, "Producto3", "Type3", "Brand3", "Color3", "Notes3");
 
-    Post post1 = new Post(1,1, LocalDate.of(2023,06,04), product1, 1, 25.50);
-    Post post2 = new Post(1,2, LocalDate.of(2023,05,28), product2, 1, 15.90);
-    Post post3 = new Post(1,3, LocalDate.of(2022,04,04), product3, 1, 190.00);
-    Post post4 = new Post(1,4, LocalDate.of(2023,05,29), product2, 1, 90.90);
+            Post post1 = new Post(1, 1, LocalDate.of(2023, 06, 04), product1, 1, 25.50);
+            Post post2 = new Post(1, 2, LocalDate.of(2023, 05, 28), product2, 1, 15.90);
+            Post post3 = new Post(1, 3, LocalDate.of(2022, 04, 04), product3, 1, 190.00);
+            Post post4 = new Post(1, 4, LocalDate.of(2023, 05, 29), product2, 1, 90.90);
 
-    List<Post> posts = new ArrayList<>();
-    posts.add(post1);
-    posts.add(post2);
-    posts.add(post3);
-    posts.add(post4);
+            List<Post> posts = Arrays.asList(post1, post2, post3, post4);
+            posts.add(post1);
+            posts.add(post2);
+            posts.add(post3);
+            posts.add(post4);
 
-    User user1 = new User(1, "Pepe", new ArrayList<>(), new ArrayList<>(), posts);
-    User user2 = new User(2, "Pablo", new ArrayList<>(), Arrays.asList(user1), new ArrayList<>());
-    User user3 = new User(3, "Pedro", new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
+            User user1 = new User(1, "Pepe", new ArrayList<>(), new ArrayList<>(), posts);
+            User user2 = new User(2, "Pablo", new ArrayList<>(), Arrays.asList(user1), new ArrayList<>());
+            User user3 = new User(3, "Pedro", new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
 
-    this.users.add(user1);
-    this.users.add(user2);
-    this.users.add(user3);
-}catch(DateTimeParseException e){
-    System.out.println("Error parseando");
-}
+            this.users.add(user1);
+            this.users.add(user2);
+            this.users.add(user3);
+        } catch (DateTimeParseException e) {
+            System.out.println("Error parseando");
+        }
     }
 
     @Override
-    public Optional<User> findUserById(int id){
+    public Optional<User> findUserById(int id) {
+
         return users.stream().filter(u -> u.getUser_id() == id).findFirst();
     }
-    public User findUser(int id){
+
+    public User findUser(int id) {
         return users.stream().filter(u -> u.getUser_id() == id).findFirst().orElse(null);
     }
-    public void addPostToUser(Post post, User user){
-        for (User usuario: users) {
-            if(usuario.getUser_id()==user.getUser_id()){
-               List<Post> posteos = usuario.getPosts();
-               posteos.add(post);
-               usuario.setPosts(posteos);
+
+    public void addPostToUser(Post post, User user) {
+        for (User usuario : users) {
+            if (usuario.getUser_id() == user.getUser_id()) {
+                List<Post> posteos = usuario.getPosts();
+                posteos.add(post);
+                usuario.setPosts(posteos);
             }
         }
     }
