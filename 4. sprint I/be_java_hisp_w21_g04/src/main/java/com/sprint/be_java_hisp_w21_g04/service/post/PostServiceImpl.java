149c0371e63f497e465930a2e68b86322a1dae33
Juan Manuel A <juanmanuel.arango@mercadolibre.com.co>
2023-06-06 18:36:50 -0500

    Agregando el limite de 2 semanas para los posts

--- 4. sprint I/be_java_hisp_w21_g04/src/main/java/com/sprint/be_java_hisp_w21_g04/service/post/PostServiceImpl.java
+++ 4. sprint I/be_java_hisp_w21_g04/src/main/java/com/sprint/be_java_hisp_w21_g04/service/post/PostServiceImpl.java
@@ -9,7 +9,9 @@ import com.sprint.be_java_hisp_w21_g04.repository.post.PostRepositoryImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLOutput;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,28 +37,37 @@ public class PostServiceImpl implements IPostService{
     
 
 //    metodo sobrecargado para prueba
-    @Override
     public SellerFollowedListPostResponseDto sellerFollowedListPosts(int user_id, String order) {
-
-//      Se ordena la lista de posts segun el parametro 'order'
+//      Se define el tiempo de publicacion de posts de las ultimas dos semanas
+//      Se define una fecha limite/base de dos semanas hacia atras desde la fecha actual
+        LocalDate twoWeeksAgo = LocalDate.now().minusWeeks(2);
+        System.out.println("HOLA" + twoWeeksAgo);
         List<PostResponseDto> posts = this._repository.getSellerFollowed(user_id).stream()
-                    .map(post -> modelMapper.map(post, PostResponseDto.class))
-                    .sorted((post1, post2) -> {
-                        LocalDate date1 = post1.getDate();
-                        LocalDate date2 = post2.getDate();
+                .filter(post -> {
+                    LocalDate postDate = post.getDate();
+//                  Se filtran los posts que tengan una fecha de publicacion mayor o igual a dos semanas
+                    return postDate.isAfter(twoWeeksAgo) || postDate.isEqual(twoWeeksAgo);
+                })
+//              Se mapea la lista de posts a una lista de PostResponseDto
+                .map(post -> modelMapper.map(post, PostResponseDto.class))
+//              Se ordena la lista de posts por fecha de publicacion
+                .sorted((post1, post2) -> {
+                    LocalDate date1 = post1.getDate();
+                    LocalDate date2 = post2.getDate();
 
-                        if ("date_asc".equals(order)) {
-                            return date1.compareTo(date2);
-                        } else if ("date_desc".equals(order)) {
-                            return date2.compareTo(date1);
-                        } else {
-                            return 0;
-                        }
-                    })
-//                  Se convierte la lista a un ArrayList
-                    .collect(Collectors.toList());
-
+//                  Se ordena la lista de posts por fecha de publicacion de forma ascendente o descendente
+                    if ("date_asc".equals(order)) {
+                        return date1.compareTo(date2);
+                    } else if ("date_desc".equals(order)) {
+                        return date2.compareTo(date1);
+                    } else {
+                        return 0;
+                    }
+                })
+//               Se convierte la lista a un ArrayList
+                .collect(Collectors.toList());
 //        System.out.println("HOLA" + posts.size());
         return new SellerFollowedListPostResponseDto(user_id, posts);
+
     }
 }
