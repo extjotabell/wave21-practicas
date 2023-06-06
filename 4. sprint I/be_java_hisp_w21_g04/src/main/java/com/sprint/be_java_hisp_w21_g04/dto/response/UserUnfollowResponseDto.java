a526ef29821e667c18434dfcef43b220c4f91df1
Charlie Schiaffino <carlos.schiaffino@mercadolibre.cl>
2023-06-06 16:09:33 -0400

    JSON Naming.

--- 4. sprint I/be_java_hisp_w21_g04/src/main/java/com/sprint/be_java_hisp_w21_g04/dto/response/UserUnfollowResponseDto.java
+++ 4. sprint I/be_java_hisp_w21_g04/src/main/java/com/sprint/be_java_hisp_w21_g04/dto/response/UserUnfollowResponseDto.java
@@ -1,5 +1,7 @@
 package com.sprint.be_java_hisp_w21_g04.dto.response;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class UserUnfollowResponseDto {
     private String message;
     private int statusCode;
