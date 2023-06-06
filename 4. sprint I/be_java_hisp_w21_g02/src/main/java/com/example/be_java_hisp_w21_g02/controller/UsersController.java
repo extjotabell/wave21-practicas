38d1819cc0381adddd5b717ea676efa272e990cb
Yanina Belen Faretta <belen.faretta@mercadolibre.com>
2023-06-06 13:28:58 -0300

    feature 8 done

--- 4. sprint I/be_java_hisp_w21_g02/src/main/java/com/example/be_java_hisp_w21_g02/controller/UsersController.java
+++ 4. sprint I/be_java_hisp_w21_g02/src/main/java/com/example/be_java_hisp_w21_g02/controller/UsersController.java
@@ -1,17 +1,22 @@
 package com.example.be_java_hisp_w21_g02.controller;
 
-import com.example.be_java_hisp_w21_g02.dto.response.FollowersCountDTO;
+import com.example.be_java_hisp_w21_g02.dto.response.FollowedListDTO;
+import com.example.be_java_hisp_w21_g02.dto.response.FollowersListDTO;
+import com.example.be_java_hisp_w21_g02.exceptions.OrderNotFoundException;
 import com.example.be_java_hisp_w21_g02.service.IUsersService;
+import com.example.be_java_hisp_w21_g02.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
     @Autowired
     private IUsersService _userService;
+
 
     @PostMapping("/{userId}/follow/{userIdToFollow}")
     public ResponseEntity<?>  followUser(@PathVariable int userId, @PathVariable int userIdToFollow){
@@ -32,16 +37,29 @@ public class UsersController {
         );
     }
     @GetMapping("/{userId}/followers/list")
-    public ResponseEntity<?> getFollowersList(@PathVariable int userId){
-        return new ResponseEntity<>(
-                _userService.getFollowersList(userId), HttpStatus.OK
-        );
+    public ResponseEntity<?> getFollowersList(@PathVariable int userId, @RequestParam(required = false) String order){
+        if (order != null && !Constants.isOrderConstant(order))
+            throw new OrderNotFoundException("El tipo de ordenamiento no existe");
+
+        FollowersListDTO followersListDTO;
+        if (order != null)
+            followersListDTO =  _userService.getFollowersList(userId, order);
+        else
+            followersListDTO = _userService.getFollowersList(userId);
+
+        return new ResponseEntity<>(followersListDTO, HttpStatus.OK);
     }
 
     @GetMapping("/{userId}/followed/list")
-    public ResponseEntity<?> getFollowedist(@PathVariable int userId){
-        return new ResponseEntity<>(
-                _userService.getFollowedList(userId), HttpStatus.OK
-        );
+    public ResponseEntity<?> getFollowedist(@PathVariable int userId, @RequestParam(required = false) String order){
+        if (order != null && !Constants.isOrderConstant(order))
+            throw new OrderNotFoundException("El tipo de ordenamiento no existe");
+
+        FollowedListDTO followedListDTO;
+        if (order != null)
+            followedListDTO =  _userService.getFollowedList(userId, order);
+        else
+            followedListDTO = _userService.getFollowedList(userId);
+        return new ResponseEntity<> (followedListDTO, HttpStatus.OK);
     }
 }
