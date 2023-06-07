ea20f5453771d380df4d0c6c953d936f3b9fed68
Camila Molina <camila.neculnir@mercadolibre.cl>
2023-06-07 13:39:45 -0400

    Delete 4. sprint I/Proyecto grupal /be_java_hisp_w21_g04 directory

--- 4. sprint I/Proyecto grupal /be_java_hisp_w21_g04/src/main/java/com/sprint/be_java_hisp_w21_g04/exception/EmptySellerFollowedList.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.be_java_hisp_w21_g04.exception;
-
-public class EmptySellerFollowedList extends RuntimeException{
-    public EmptySellerFollowedList(String message){
-        super(message);
-    }
-}
