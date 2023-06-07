de2718d7ec9149bb6260a0149bb0dd6734a675e7
ddelavegaML <demian.delavega@mercadolibre.com>
2023-06-07 15:02:55 -0300

    Creacion y manejo dto de errores.

--- 4. sprint I/be_java_hisp_w21_g1_de_la_vega/src/main/java/com/example/be_java_hisp_w21_g1/Exception/ExceptionsController.java
+++ 4. sprint I/be_java_hisp_w21_g1_de_la_vega/src/main/java/com/example/be_java_hisp_w21_g1/Exception/ExceptionsController.java
@@ -1,5 +1,6 @@
 package com.example.be_java_hisp_w21_g1.Exception;
 
+import com.example.be_java_hisp_w21_g1.DTO.Error.ErrorDTO;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,19 +11,22 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class ExceptionsController extends ResponseEntityExceptionHandler {
-
+    private ErrorDTO errorDTO = new ErrorDTO();
     @ExceptionHandler(NotFoundException.class)
     protected ResponseEntity<Object> notFoundException(Exception e, WebRequest request){
-        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        errorDTO.setMessage(e.getMessage());
+        return handleExceptionInternal(e, errorDTO, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler(BadRequestException.class)
     protected ResponseEntity<Object> badRequestException(Exception e, WebRequest request){
-        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        errorDTO.setMessage(e.getMessage());
+        return handleExceptionInternal(e, errorDTO, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler(UserNotSellerException.class)
     protected ResponseEntity<Object> userNotSellerException(Exception e, WebRequest request){
-        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        errorDTO.setMessage(e.getMessage());
+        return handleExceptionInternal(e, errorDTO, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 }
