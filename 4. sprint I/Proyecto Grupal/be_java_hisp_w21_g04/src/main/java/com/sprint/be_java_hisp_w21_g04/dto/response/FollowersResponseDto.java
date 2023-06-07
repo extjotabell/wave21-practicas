234c69b0e08fdc39cc69208bb786722b99e4c8d7
Camila Molina <camila.neculnir@mercadolibre.cl>
2023-06-07 13:50:30 -0400

    Se agrega el proyecto grupal

---
+++ 4. sprint I/Proyecto Grupal/be_java_hisp_w21_g04/src/main/java/com/sprint/be_java_hisp_w21_g04/dto/response/FollowersResponseDto.java
@@ -0,0 +1,22 @@
+package com.sprint.be_java_hisp_w21_g04.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class FollowersResponseDto {
+
+    private int userId;
+    private String userName;
+    private List<UserResponseDto> followers;
+
+}
---
+++ 4. sprint I/Proyecto Grupal/be_java_hisp_w21_g04/src/main/java/com/sprint/be_java_hisp_w21_g04/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.sprint.be_java_hisp_w21_g04.exception;
+
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
