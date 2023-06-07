fd9588de6530ea3825d041546e49dd2b321e47f2
Yanina Belen Faretta <belen.faretta@mercadolibre.com>
2023-06-07 17:15:57 -0300

    adding javadoc and postman

--- 4. sprint I/be_java_hisp_w21_g02_faretta/src/main/java/com/example/be_java_hisp_w21_g02/service/IProductsService.java
+++ 4. sprint I/be_java_hisp_w21_g02_faretta/src/main/java/com/example/be_java_hisp_w21_g02/service/IProductsService.java
@@ -11,9 +11,25 @@ public interface IProductsService {
 
     ResponseEntity<?> listFollowingPosts2Weeks(int userId, String order);
 
+    /**
+     * @param promoPostRequestDTO DTO with the promo post data
+     * @return 200 if the post is a promo (has has_promo and discount) and can be created.
+     * If the data isn't valid returns 400.
+     */
     ResponseEntity<?> createPost(PromoPostRequestDTO promoPostRequestDTO);
 
+    /**
+     * @param userId
+     * @return 200 if the id is valid and a body with the user data and its post count.
+     * If the data isn't valid returns 400.
+     */
     ResponseEntity<?> countPromoPosts(int userId);
 
+    /**
+     * @param userId
+     * @param postId
+     * @return 200 if the ids are valids and the post can be removed.
+     * If the data isn't valid returns 400.
+     */
     ResponseEntity<?> removePost(int userId, int postId);
 }
