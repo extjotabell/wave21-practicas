0e7d4c0476d4d1e297ba021ab1059fe24aec7d75
Camila Molina <camila.neculnir@mercadolibre.cl>
2023-06-07 13:55:39 -0400

    Delete 4. sprint I/Proyecto Grupal/Proyecto Individual directory

--- 4. sprint I/Proyecto Grupal/Proyecto Individual/be_java_hisp_w21_g04/src/main/java/com/sprint/be_java_hisp_w21_g04/exception/PostAlreadyExist.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.be_java_hisp_w21_g04.exception;
-
-public class PostAlreadyExist extends RuntimeException{
-    public PostAlreadyExist(String message){
-        super(message);
-    }
-}
