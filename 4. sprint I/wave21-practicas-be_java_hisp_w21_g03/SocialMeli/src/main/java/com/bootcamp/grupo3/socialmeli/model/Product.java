86577f4610976e458cd62c2498c6ac1249fbfc99
gantonietti <gabriel.antonietti@mercadolibre.com>
2023-06-07 15:18:08 -0300

    subiendo carpetas parte 1

---
+++ 4. sprint I/wave21-practicas-be_java_hisp_w21_g03/SocialMeli/src/main/java/com/bootcamp/grupo3/socialmeli/model/Product.java
@@ -0,0 +1,15 @@
+package com.bootcamp.grupo3.socialmeli.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Product {
+    private int productId;
+    private String productName;
+    private String type;
+    private String brand;
+    private String color;
+    private String notes;
+}
