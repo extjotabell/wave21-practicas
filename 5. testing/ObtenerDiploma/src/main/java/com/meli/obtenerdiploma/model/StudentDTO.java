e3800178e8aae1b06561c672ebc315d5b8d3b90f
AdrianRodriguezML <adrianrodriguez0510@gmail.com>
2023-06-09 17:02:04 -0300

    ejercicio validaciones

--- 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -1,15 +1,27 @@
 package com.meli.obtenerdiploma.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Getter @Setter
+@EqualsAndHashCode
+
 public class StudentDTO {
+
+    @NotBlank(message="Nombre no puede ser vacio")
+    @Size(max = 50, message = "El largo del nombre no puede ser mayor a 50")
+    @Pattern(regexp = "^[A-Z]. $", message = "El nombre de estudiante debe comenzar con mayuscula")
     String studentName;
+
     String message;
     Double averageScore;
+
+    @NotEmpty(message = "La lista no puede ser vacia")
     List<SubjectDTO> subjects;
 }
--- 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
@@ -3,8 +3,19 @@ package com.meli.obtenerdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
+
+import javax.validation.constraints.*;
+
 @Getter @Setter
 public class SubjectDTO {
+
+    @NotBlank(message = "El nombre no puede ser vacio")
+    @Size(message = "El largo del nombre no puede ser mayor a 30", max = 30)
+    @Pattern(regexp = "^[A-Z]. $", message = "El nombre de la materia debe comenzar con mayuscula")
     String name;
+
+    @NotBlank(message = "El score no puede ser vacio")
+    @Min(value=0, message = "El minimo de score a ingresar es 0")
+    @Max(value=10, message = "El maximo de score a ingresar es 10")
     Double score;
 }
