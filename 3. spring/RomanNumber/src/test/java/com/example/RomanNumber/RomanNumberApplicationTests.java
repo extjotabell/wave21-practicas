c65503228598bbf648ae15fada618da1db7e675b
flecaroos <francisco.lecaros@mercadolibre.cl>
2023-06-09 15:04:09 -0400

    Ejercicio de numeros romanos

---
+++ 3. spring/RomanNumber/src/test/java/com/example/RomanNumber/RomanNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RomanNumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanNumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
