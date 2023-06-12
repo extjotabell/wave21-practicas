96535a724fe712b431f6b22d3373de64af266f26
Charlie Schiaffino <carlos.schiaffino@mercadolibre.cl>
2023-06-12 10:45:08 -0400

    Otros tests agregados para aumentar coverage.

--- 5. testing/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/controller/StudentControllerTests.java
+++ 5. testing/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/controller/StudentControllerTests.java
@@ -1,7 +1,33 @@
 package com.meli.obtenerdiploma.controller;
 
+import com.meli.obtenerdiploma.model.StudentDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
 @AutoConfigureMockMvc
 public class StudentControllerTests {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getStudentByIdTest() throws Exception{
+        MvcResult mvcResult = mockMvc.perform(get("/student/getStudent/{studentId}", 1L))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.studentName").value("Juan"))
+                .andReturn();
+    }
 }
+
