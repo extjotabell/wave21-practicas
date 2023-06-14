e9ddd9b5322c4795ad7c6a7c3e5c62b1d850f1da
Dominguez Gaston Matias <gaston.gdominguez@mercadolibre.com>
2023-06-14 15:32:17 -0300

    add project roman numbers

---
+++ 5. testing/2. b. roman-numerals/src/test/java/com/mercadolibre/romannumerals/RomanNumeralsRestControllerTest.java
@@ -0,0 +1,26 @@
+package com.mercadolibre.romannumerals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class RomanNumeralsRestControllerTest {
+
+    RomanNumeralsRestController controller=new RomanNumeralsRestController();
+
+    @Test
+    void test1ToRoman(){
+        runTest(1,"I");
+    }
+
+    @Test
+    void test3554ToRoman(){
+        runTest(3554,"MMMDLIV");
+    }
+    private void runTest(int number, String roman){
+        String result = controller.toRoman(number);
+        assertEquals(roman,result);
+    }
+}   
