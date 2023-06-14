7949358fbaed256e134e21507f64d4a113b54fb9
Mercedes Magnelli <mercedes.magnelli@mercadolibre.com>
2023-06-14 15:30:25 -0300

    no se como testear el repositorio

--- 5. testing/ObtenerDiploma - con y sin mocks/src/test/java/com/meli/obtenerdiploma/Repository/StudentRepositoryTests.java
+++ 5. testing/ObtenerDiploma - con y sin mocks/src/test/java/com/meli/obtenerdiploma/Repository/StudentRepositoryTests.java
@@ -4,39 +4,35 @@ import com.meli.obtenerdiploma.model.StudentDTO;
 import com.meli.obtenerdiploma.model.SubjectDTO;
 import com.meli.obtenerdiploma.repository.StudentDAO;
 import com.meli.obtenerdiploma.repository.StudentRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 
 public class StudentRepositoryTests {
+    static StudentRepository repository;
 
-    /*Se requiere crear los tests unitarios necesarios para cubrir el
-     comportamiento de la capa de repositorio StudentDAO y StudentRepository.
-     Tener en cuenta múltiples escenarios y “casos borde” de cada comportamiento.
-    Casos nulos, vacíos, inválidos.
-    Agregar un alumno.
-    Buscar un alumno por Id.
-    Modificar los datos de un alumno.
-    Eliminar un alumno.
-    Listar todos los alumnos.
-*/
+    @BeforeAll
+    public static void beforeAll() {
+        repository = new StudentRepository();
+    }
+
     @Test
     //esto esta correctamente testeado?
     public void allStudentsAreOk() {
-        StudentRepository repository = new StudentRepository();
+       //Arrange
         Set<StudentDTO> actual = repository.findAll();
 
-
-        List<StudentDTO> expected = new ArrayList<>();
-        expected.add(new StudentDTO(1L, "Juan", null,null,List.of(new SubjectDTO("Matemática", 9d), new SubjectDTO("Física", 7d), new SubjectDTO("Química",6d))));
-        expected.add(new StudentDTO(2L, "Pedro", null,null,List.of(new SubjectDTO("Matemática", 10d), new SubjectDTO("Física", 8d), new SubjectDTO("Química",4d))));
+        //Act -> no tiene ninguna
+        Set<StudentDTO> expected = new HashSet<>();
+        //Assert
 
         //assert
-        //assertEquals(null, expected.toString(),actual.toString());
+        Assertions.assertEquals(expected, actual);
 
 
     }
@@ -45,6 +41,13 @@ public class StudentRepositoryTests {
     @Test
     public void fileIsNotFound() {
 
+
+        //Act -> no tiene ninguna
+
+        //Assert
+
+        //assert
+        Assertions.assertThrows(() -> FileNotFoundException.class, repository.findAll());
     }
 
     //me falta probar la otra excepcion pero no sé como generarla
