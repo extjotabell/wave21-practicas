50a7dad72f96032a9b22c4ceed5b611f3f94820b
gbarro <gaston.barro@mercadolibre.com>
2023-06-15 17:56:31 -0300

    Agrego primer test de integración y un usuario a la base Json

---
+++ 5. testing/Alumnos/src/test/java/com/demospring/demo/integrationtest/AlumnoControllerTest.java
@@ -0,0 +1,34 @@
+package com.demospring.demo.integrationtest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AlumnoControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Integration Test 1 - Get all")
+    public void getAllOK () throws Exception {
+        mockMvc.perform(get("/alumnos/getAll"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+    }
+
+}
