11bc0daf74ff3896b84fe02475712a41a5afd0e5
flarodriguez <flavio.rodriguez@mercadolibre.com>
2023-06-15 18:19:42 -0300

    Mejorando obtener Diploma

--- 5. testing/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/controller/StudentControllerIntegrationTest.java
+++ 5. testing/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/controller/StudentControllerIntegrationTest.java
@@ -1,103 +1,106 @@
 package com.meli.obtenerdiploma.controller;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.meli.obtenerdiploma.model.StudentDTO;
 import com.meli.obtenerdiploma.model.SubjectDTO;
 import com.meli.obtenerdiploma.repository.StudentDAO;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+
+import com.meli.obtenerdiploma.service.IStudentService;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.io.UnsupportedEncodingException;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 public class StudentControllerIntegrationTest {
 
 
     @Autowired
     MockMvc mockMvc;
 
-    @BeforeAll
-    static void beforeAll() {
+    @Mock
+    StudentDAO studentDao;
 
-        /*StudentDAO repo = new StudentDAO();
-        SubjectDTO subject1 = new SubjectDTO("Math", 10D);
-        SubjectDTO subject2 = new SubjectDTO("Science", 7D);
-        SubjectDTO subject3 = new SubjectDTO("History", 9D);
-        SubjectDTO subject4 = new SubjectDTO("Biology", 5D);
+    @Mock
+    private IStudentService studentService;
 
-        List<SubjectDTO> subjects1 = Arrays.asList(subject1,subject2,subject3,subject4);
-        List<SubjectDTO> subjects2 = Arrays.asList(subject1,subject4);
+    @InjectMocks
+    private StudentController studentController;
 
+    private StudentDTO createStudentDTO() {
+        StudentDTO studentDTO = new StudentDTO();
+        studentDTO.setId(1L);
+        studentDTO.setStudentName("John Doe");
+        studentDTO.setMessage("Congratulations!");
+        studentDTO.setAverageScore(9.5);
+        List<SubjectDTO> subjects = new ArrayList<>();
+        subjects.add(createSubjectDTO("Mathematics", 8.5));
+        subjects.add(createSubjectDTO("Physics", 9.0));
+        subjects.add(createSubjectDTO("Chemistry", 9.5));
+        studentDTO.setSubjects(subjects);
+        return studentDTO;
+    }
 
-        StudentDTO studentDto = new StudentDTO(null, "Juan",null,null, new ArrayList<>());
-        studentDto.setSubjects(subjects1);
-        StudentDTO studentDto2 = new StudentDTO(null, "Pepe",null,null, new ArrayList<>());
-        studentDto2.setSubjects(subjects2);
-
-
-        repo.save(studentDto);
-        repo.save(studentDto2);*/
-
-
+    private SubjectDTO createSubjectDTO(String name, Double score) {
+        SubjectDTO subjectDTO = new SubjectDTO();
+        subjectDTO.setName(name);
+        subjectDTO.setScore(score);
+        return subjectDTO;
     }
 
 
-    // /registerStudent
+
+
+// /registerStudent
     @Test
-    public void testIntegrationRegisterStudent() throws Exception {
+    void testRegisterStudent() {
+        // Arrange
+        StudentDTO studentDTO = createStudentDTO();
 
-        StudentDTO studentDto = new StudentDTO(10L, "Juan",null,null, new ArrayList<>());
+        // Act
+        ResponseEntity<?> response = studentController.registerStudent(studentDTO);
 
-
-        ObjectWriter writer = new ObjectMapper()
-                .configure(SerializationFeature.WRAP_ROOT_VALUE,false)
-                .writer();
-
-        String payloadDto = writer.writeValueAsString(studentDto);
-        MvcResult mvcResult = mockMvc.perform(post("/student/registerStudent")
-                        .contentType(APPLICATION_JSON)
-                        .content(payloadDto))
-                .andDo(print())
-                .andExpect(content().contentType("application/json"))
-                .andReturn();
-
-        assertEquals(payloadDto,mvcResult.getResponse().getContentAsString());
+        // Assert
+        assertEquals(ResponseEntity.ok(null), response);
+        verify(studentService, times(1)).create(studentDTO);
 
     }
 
     @Test
     // /getStudent/{id}
-    public void testIntegrationGetStudent() throws Exception{
+    void testIntegrationGetStudent() throws Exception{
         StudentDAO repo = new StudentDAO();
         SubjectDTO subject1 = new SubjectDTO("Math", 10D);
         SubjectDTO subject2 = new SubjectDTO("Science", 7D);
@@ -136,16 +139,18 @@ public class StudentControllerIntegrationTest {
 
     ///removeStudent/{id}
     @Test
-    public void testIntegrationRemoveStudent() throws Exception{
-        //"/analyzeScores/{studentId}
-        MvcResult mvcResult = mockMvc.perform(get("/student/removeStudent/{id}",1))
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.ret").value(true))
-                .andReturn();
+    void testIntegrationRemoveStudent() throws Exception {
+            int id = 1;
+
+            var request = get("/student/removeStudent/"+id);
+            MvcResult result = mockMvc.perform(request).andDo(print()).andReturn();
+
+            assertEquals(200, result.getResponse().getStatus());
+
     }
 
-    // /listStudents
+    //Agregar list students
+
 
 
 }
