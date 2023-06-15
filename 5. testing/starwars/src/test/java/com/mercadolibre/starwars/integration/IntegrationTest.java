ea6f2cf3eb9543f3a662868d4b99b65d595c3bf5
ezesafdie-meli <ezequiel.safdie@mercadolibre.com>
2023-06-15 18:01:02 -0300

    Tests basicos de integracion y unitarios

--- 5. testing/starwars/src/test/java/com/mercadolibre/starwars/integration/IntegrationTest.java
+++ 5. testing/starwars/src/test/java/com/mercadolibre/starwars/integration/IntegrationTest.java
@@ -1,4 +1,32 @@
 package com.mercadolibre.starwars.integration;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class IntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Integration Test find character by query")
+    void mainTest() throws Exception {
+        this.mockMvc.perform(
+                MockMvcRequestBuilders.get("/{query}", "luke"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$[0].name").value("Luke Skywalker"));
+    }
+
 }
--- 5. testing/starwars/src/test/java/com/mercadolibre/starwars/service/FindServiceTest.java
+++ 5. testing/starwars/src/test/java/com/mercadolibre/starwars/service/FindServiceTest.java
@@ -1,2 +1,8 @@
-package com.mercadolibre.starwars.service;public class FindServiceTest {
+package com.mercadolibre.starwars.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class FindServiceTest {
 }
