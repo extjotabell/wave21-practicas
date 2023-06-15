64b46f7c9f21a1513fc4833035b6a71658af3fc6
gantonietti <gabriel.antonietti@mercadolibre.com>
2023-06-15 17:47:58 -0300

    eliminando proyecto obtenerDiploma roto

---
+++ 5. testing/LiveCodingTest/src/main/java/com/example/livecodingtest/dto/TestPostDto.java
@@ -0,0 +1,12 @@
+package com.example.livecodingtest.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class TestPostDto {
+    private String name;
+}
