35a1541c3f28b235efce006f436a8adb48242df4
nhdebellis <nahuel.bellis@mercadolibre.com>
2023-06-16 15:59:31 -0300

    rollback de el sprint

--- 4. sprint I/SocialMeli/src/main/java/com/bootcamp/grupo3/socialmeli/controller/PostController.java
+++ 4. sprint I/SocialMeli/src/main/java/com/bootcamp/grupo3/socialmeli/controller/PostController.java
@@ -3,9 +3,7 @@ package com.bootcamp.grupo3.socialmeli.controller;
 import com.bootcamp.grupo3.socialmeli.dto.request.PostDTO;
 import com.bootcamp.grupo3.socialmeli.dto.response.MessageDTO;
 import com.bootcamp.grupo3.socialmeli.service.interfaces.IPostService;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.bootcamp.grupo3.socialmeli.dto.response.UserPostListDTO;
 import com.bootcamp.grupo3.socialmeli.service.interfaces.IUserService;
@@ -13,7 +11,6 @@ import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("/products")
-@Validated
 public class PostController {
     private IPostService postService;
 
@@ -22,15 +19,12 @@ public class PostController {
     }
 
     @PostMapping("/post")
-    public ResponseEntity<MessageDTO> createPost(@Valid @RequestBody PostDTO body) {
+    public ResponseEntity<MessageDTO> createPost(@RequestBody PostDTO body) {
         int postId = postService.createPost(body);
         return ResponseEntity.ok(new MessageDTO("Post agregado exitosamente con id: " + postId));
     }
     @GetMapping("/followed/{userId}/list")
-    public ResponseEntity<UserPostListDTO> listPostOfFollowers(
-            @PathVariable final int userId,
-            @RequestParam(required = false) final String order
-    ) {
+    public ResponseEntity<UserPostListDTO> listPostOfFollowers(@PathVariable final int userId, @RequestParam(required = false) final String order) {
         UserPostListDTO posts = postService.getPostList(userId, order);
 
         return new ResponseEntity<>(posts, HttpStatus.OK);
--- 4. sprint I/SocialMeli/src/main/java/com/bootcamp/grupo3/socialmeli/dto/request/PostDTO.java
+++ 4. sprint I/SocialMeli/src/main/java/com/bootcamp/grupo3/socialmeli/dto/request/PostDTO.java
@@ -3,16 +3,10 @@ package com.bootcamp.grupo3.socialmeli.dto.request;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.validator.constraints.Range;
 
 import java.time.LocalDate;
 
@@ -20,24 +14,11 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
-public class  PostDTO {
-
-    @NotNull(message = "El id del usuario no puede estar vacío.")
-    @Positive(message = "El id del usuario debe ser mayor a cero")
-    private Integer userId;
-
+public class    PostDTO {
+    private int userId;
     @JsonFormat(pattern="dd-MM-yyyy")
-    @NotNull(message = "La fecha no puede estar vacía.")
     private LocalDate date;
-
-    @Valid
-    @NotNull(message = "El producto no puede estar vacío.")
     private ProductDTO product;
-
-    @NotNull(message = "La categoría no puede estar vacía.")
-    private Integer category;
-
-    @NotNull(message = "El precio no puede estar vacío.")
-    @Max(value=10_000_000, message = "El precio máximo por producto es de 10.000.000")
-    private Double price;
+    private int category;
+    private double price;
 }
