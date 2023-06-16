0b8b19e3661e47479e957b252bb8e39492c63e0e
ddelavegaML <demian.delavega@mercadolibre.com>
2023-06-16 15:51:48 -0300

    Merge y solucion de conflictos. Test unitario (T-0004) para followers en ordenamiento ascendente y descendente correctos.

--- 6. sprint II/be_java_hisp_w21_g1/src/test/java/com/example/be_java_hisp_w21_g1/unitary/UserServiceTest.java
+++ 6. sprint II/be_java_hisp_w21_g1/src/test/java/com/example/be_java_hisp_w21_g1/unitary/UserServiceTest.java
@@ -1,13 +1,12 @@
 package com.example.be_java_hisp_w21_g1.unitary;
 
 import com.example.be_java_hisp_w21_g1.DTO.Request.FollowPostDTO;
+import com.example.be_java_hisp_w21_g1.DTO.Response.FollowerListDTO;
 import com.example.be_java_hisp_w21_g1.Exception.NotFoundException;
 import com.example.be_java_hisp_w21_g1.Model.Post;
 import com.example.be_java_hisp_w21_g1.Model.Product;
 import com.example.be_java_hisp_w21_g1.Model.User;
 import com.example.be_java_hisp_w21_g1.Repository.IUserRepository;
-import com.example.be_java_hisp_w21_g1.Repository.UserRepository;
-import com.example.be_java_hisp_w21_g1.Service.IUserService;
 import com.example.be_java_hisp_w21_g1.Service.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -15,7 +14,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
@@ -27,10 +32,12 @@ import static org.mockito.Mockito.when;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest
+
 public class UserServiceTest {
+
     @Mock
     IUserRepository userRepository;
+
     @InjectMocks
     UserService userService;
 
@@ -79,7 +86,7 @@ public class UserServiceTest {
         });
 
     }
-
+    
     @Test
     @DisplayName("US-0003 - Happy path :) ")
     public void orderListOk() {
@@ -100,7 +107,6 @@ public class UserServiceTest {
 
     }
 
-
     @Test
     @DisplayName("T-0007 - Sad Path D:")
     void followerCountThrowsException(){
@@ -108,11 +114,74 @@ public class UserServiceTest {
 
     }
 
+    @Test
+    @DisplayName("T-0004 - Verificar el correcto ordenamiento ascendente por nombre. (US-0008)")
+    void getFollowersListAscOk(){
+        //ARRANGE
+        int idUser = 1;
+        String order = "name_asc";
+        User follower1 = new User();
+        User follower2 = new User();
+        follower1.setUser_id(2);
+        follower1.setUser_name("Andrea");
+        follower2.setUser_id(3);
+        follower2.setUser_name("Santiago");
 
+        Optional<User> userMock = Optional.of(new User(
+                idUser,
+                "Pepe",
+                List.of(follower2,follower1),
+                List.of(),
+                List.of()
+        ));
 
+        //MOCK
+        Mockito
+                .when(userRepository.findUserById(idUser)).thenReturn(userMock);
 
+        //ACT
+        FollowerListDTO result = userService.getFollowersList(idUser, order);
 
+        //ASSERT
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(2, result.getFollowers().get(0).getUser_id());
+        Assertions.assertEquals(3, result.getFollowers().get(1).getUser_id());
 
+    }
 
+    @Test
+    @DisplayName("T-0004 - Verificar el correcto ordenamiento descendente por nombre. (US-0008)")
+    void getFollowersListDescOk(){
+        //ARRANGE
+        int idUser = 1;
+        String order = "name_desc";
+        User follower1 = new User();
+        User follower2 = new User();
+        follower1.setUser_id(2);
+        follower1.setUser_name("Andrea");
+        follower2.setUser_id(3);
+        follower2.setUser_name("Santiago");
+
+        Optional<User> userMock = Optional.of(new User(
+                idUser,
+                "Pepe",
+                List.of(follower2,follower1),
+                List.of(),
+                List.of()
+        ));
+
+        //MOCK
+        Mockito
+                .when(userRepository.findUserById(idUser)).thenReturn(userMock);
+
+        //ACT
+        FollowerListDTO result = userService.getFollowersList(idUser, order);
+
+        //ASSERT
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(3, result.getFollowers().get(0).getUser_id());
+        Assertions.assertEquals(2, result.getFollowers().get(1).getUser_id());
+
+    }
 
 }
