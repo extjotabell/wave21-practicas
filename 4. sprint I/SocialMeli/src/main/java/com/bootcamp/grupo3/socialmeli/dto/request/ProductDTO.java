f41289132be985e4e5d90b9c5cb4c1fbf8b1ef27
luchoLattante <luciano.lattante@mercadolibre.com>
2023-06-16 13:26:00 -0300

    validations

--- 4. sprint I/SocialMeli/src/main/java/com/bootcamp/grupo3/socialmeli/dto/request/ProductDTO.java
+++ 4. sprint I/SocialMeli/src/main/java/com/bootcamp/grupo3/socialmeli/dto/request/ProductDTO.java
@@ -6,17 +6,43 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@Validated
 public class ProductDTO {
+
+    @NotNull(message = "El id no puede estar vacía.")
+    @Positive(message = "El id debe ser mayor a cero")
     private int productId;
+
+    @NotEmpty( message = "El nombre del producto no puede estar vacío.")
+    @Size(max=40, message = "La longitud no puede superar los 40 caracteres.")
+    @Pattern(regexp =  "[\\w\\s]+", message = "El nombre no puede poseer caracteres especiales.")
     private String productName;
+
+    @NotEmpty( message = "El tipo no puede estar vacío.")
+    @Size(max = 15, message = "La longitud no puede superar los 15 caracteres.")
+    @Pattern(regexp =  "[\\w\\s]+", message = "El tipo no puede poseer caracteres especiales.")
     private String type;
+
+    @Size(max=25, message = "La longitud no puede superar los 25 caracteres.")
+    @NotEmpty( message = "La marca no puede estar vacía.")
+    @Pattern(regexp =  "[\\w\\s]+", message = "La marca no puede poseer caracteres especiales.")
     private String brand;
+
+    @NotEmpty( message = "El color no puede estar vacío.")
+    @Size(max = 15, message = "La longitud no puede superar los 15 caracteres.")
+    @Pattern(regexp =  "[\\w\\s]+", message = "El color no puede poseer caracteres especiales.")
     private String color;
+
+    @Size(max = 80, message = "La longitud no puede superar los 80 caracteres.")
+    @Pattern(regexp =  "[\\w\\s]+", message = "Las notas no pueden poseer caracteres especiales.")
     private String notes;
 }
