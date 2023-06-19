5bcccc4e1eeebb20696a1a6a0f438ae6bd7543ed
José Luis <joseluis.gonzalez@mercadolibre.cl>
2023-06-19 14:21:29 -0400

    Adding refactor map DTOs on PostDTO

--- 6. sprint II/be_java_hisp_w21_g02/src/main/java/com/example/be_java_hisp_w21_g02/dto/PostDTO.java
+++ 6. sprint II/be_java_hisp_w21_g02/src/main/java/com/example/be_java_hisp_w21_g02/dto/PostDTO.java
@@ -1,5 +1,6 @@
 package com.example.be_java_hisp_w21_g02.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 public class PostDTO {
     private Integer userId;
     private Integer postId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate date;
     private ProductDTO product;
     private Integer category;
