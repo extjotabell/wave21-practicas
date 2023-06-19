7aebf40d70d9e83bb0659bb8843fa428b870c584
José Luis <joseluis.gonzalez@mercadolibre.cl>
2023-06-19 14:22:18 -0400

    Adding jsonformat to include localdate pattern

--- 6. sprint II/be_java_hisp_w21_g02/src/main/java/com/example/be_java_hisp_w21_g02/model/Post.java
+++ 6. sprint II/be_java_hisp_w21_g02/src/main/java/com/example/be_java_hisp_w21_g02/model/Post.java
@@ -1,5 +1,6 @@
 package com.example.be_java_hisp_w21_g02.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 public class Post {
     private Integer userId;
     private Integer postId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private LocalDate date;
     private Integer category;
     private Double price;
