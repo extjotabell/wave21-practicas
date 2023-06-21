b7fabe57c605ca9774dfb2d4a57c57de6bbaea71
Dominguez Gaston Matias <gaston.gdominguez@mercadolibre.com>
2023-06-21 14:28:59 -0300

    add new exception handler to catch validation errors

--- 6. sprint II/be_java_hisp_w21_g1/src/main/java/com/example/be_java_hisp_w21_g1/Exception/ExceptionsController.java
+++ 6. sprint II/be_java_hisp_w21_g1/src/main/java/com/example/be_java_hisp_w21_g1/Exception/ExceptionsController.java
@@ -1,38 +1,61 @@
 package com.example.be_java_hisp_w21_g1.Exception;
 
 import com.example.be_java_hisp_w21_g1.DTO.Error.ErrorDTO;
+import com.example.be_java_hisp_w21_g1.DTO.Error.ExceptionDTO;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
+
 import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class ExceptionsController extends ResponseEntityExceptionHandler {
-    private  ErrorDTO errorDTO = new ErrorDTO();
+public class ExceptionsController {
+    private ErrorDTO errorDTO = new ErrorDTO();
+
     @ExceptionHandler(NotFoundException.class)
-    protected ResponseEntity<Object> notFoundException(NotFoundException e, WebRequest request){
-        errorDTO.setExplanation(e.getMessage());
-        return handleExceptionInternal(e, errorDTO, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    protected ResponseEntity<Object> notFoundException(Exception e) {
+        errorDTO.setMessage(e.getMessage());
+        return new ResponseEntity<>(errorDTO, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(BadRequestException.class)
-    protected ResponseEntity<Object> badRequestException(BadRequestException e, WebRequest request){
-        errorDTO.setExplanation(e.getMessage());
-        return handleExceptionInternal(e, errorDTO, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    protected ResponseEntity<Object> badRequestException(Exception e) {
+        errorDTO.setMessage(e.getMessage());
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotSellerException.class)
-    protected ResponseEntity<Object> userNotSellerException(UserNotSellerException e, WebRequest request){
-        errorDTO.setExplanation(e.getMessage());
-        return handleExceptionInternal(e, errorDTO, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    protected ResponseEntity<Object> userNotSellerException(Exception e) {
+        errorDTO.setMessage(e.getMessage());
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ExceptionDTO> validationException(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(
+                new ExceptionDTO("Se encontraron los siguientes errores: ",
+                        e.getAllErrors()
+                                .stream()
+                                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                                .toList()
+                )
+        );
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    protected ResponseEntity<ExceptionDTO> validationException(ConstraintViolationException e) {
+        return ResponseEntity.badRequest().body(
+                new ExceptionDTO("Se encontraron los siguientes errores: ",
+                        e.getConstraintViolations()
+                                .stream()
+                                .map(ConstraintViolation::getMessage)
+                                .collect(Collectors.toList())
+                )
+        );
     }
 }
