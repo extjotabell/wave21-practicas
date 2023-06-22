39f062fb43b0e1cc8503bb898f5a5d2d5e724ac9
Fabrizzio <fabrizzio.camera@mercadolibre.com>
2023-06-22 15:52:39 -0300

    se agrega duplicado para parte individual del sprint II

---
+++ 6. sprint II/be_java_hisp_w21_g03_camera/SocialMeli2/src/main/java/com/bootcamp/grupo3/socialmeli/controller/PostController.java
@@ -0,0 +1,38 @@
+package com.bootcamp.grupo3.socialmeli.controller;
+
+import com.bootcamp.grupo3.socialmeli.dto.request.PostDTO;
+import com.bootcamp.grupo3.socialmeli.dto.response.MessageDTO;
+import com.bootcamp.grupo3.socialmeli.service.interfaces.IPostService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import com.bootcamp.grupo3.socialmeli.dto.response.UserPostListDTO;
+import com.bootcamp.grupo3.socialmeli.service.interfaces.IUserService;
+import org.springframework.http.HttpStatus;
+
+@RestController
+@RequestMapping("/products")
+@Validated
+public class PostController {
+    private IPostService postService;
+
+    public PostController(IPostService postService, IUserService userService){
+        this.postService = postService;
+    }
+
+    @PostMapping("/post")
+    public ResponseEntity<MessageDTO> createPost(@Valid @RequestBody PostDTO body) {
+        int postId = postService.createPost(body);
+        return ResponseEntity.ok(new MessageDTO("Post agregado exitosamente con id: " + postId));
+    }
+    @GetMapping("/followed/{userId}/list")
+    public ResponseEntity<UserPostListDTO> listPostOfFollowers(
+            @PathVariable final int userId,
+            @RequestParam(required = false) final String order
+    ) {
+        UserPostListDTO posts = postService.getPostList(userId, order);
+
+        return new ResponseEntity<>(posts, HttpStatus.OK);
+    }
+}
