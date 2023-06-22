a4d4000e6bb3212dae60bce19b159066ca6436c7
gantonietti <gabriel.antonietti@mercadolibre.com>
2023-06-22 13:03:31 -0300

    proyecto grupal terminado

---
+++ 5. testing/SocialMeliBaseParaTestear/src/main/java/com/bootcamp/grupo3/socialmeli/model/Post.java
@@ -0,0 +1,20 @@
+package com.bootcamp.grupo3.socialmeli.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    private int id;
+    private int userId;
+    private LocalDate date;
+    private Product product;
+    private int category;
+    private double price;
+}
---
+++ 5. testing/SocialMeliBaseParaTestear/src/test/java/com/bootcamp/grupo3/socialmeli/service/UserServiceTest.java
@@ -0,0 +1,4 @@
+package com.bootcamp.grupo3.socialmeli.service;
+
+public class UserServiceTest {
+}
