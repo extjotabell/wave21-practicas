b2c8155ba9f724eeee607d14dc28f020a2d40e49
Mercedes Magnelli <mercedes.magnelli@mercadolibre.com>
2023-06-22 16:38:32 -0300

    US03 con ambos caminos funcionando

--- 6. sprint II/be_java_hisp_w21_g1/src/main/java/com/example/be_java_hisp_w21_g1/DTO/Error/ErrorDTO.java
+++ 6. sprint II/be_java_hisp_w21_g1/src/main/java/com/example/be_java_hisp_w21_g1/DTO/Error/ErrorDTO.java
@@ -11,4 +11,5 @@ import lombok.Setter;
 @Setter
 public class ErrorDTO {
     String message;
+
 }
--- 6. sprint II/be_java_hisp_w21_g1/src/test/java/com/example/be_java_hisp_w21_g1/integration/UserControllerTests.java
+++ 6. sprint II/be_java_hisp_w21_g1/src/test/java/com/example/be_java_hisp_w21_g1/integration/UserControllerTests.java
@@ -1,5 +1,9 @@
 package com.example.be_java_hisp_w21_g1.integration;
 
+import com.example.be_java_hisp_w21_g1.DTO.Error.ErrorDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,9 +16,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -44,5 +50,27 @@ public class UserControllerTests {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.followers[0].user_name").value("Pablo"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.followers[0].user_id").value(2));
     }
+
+    @Test
+    @DisplayName("[US - 0003] - Getting followers list - Unhappy Path")
+    public void getFollowerListForANonExisting() throws Exception {
+        int nonExistingId = 99;
+
+        ErrorDTO error = new ErrorDTO("No se encontro el usuario con el ID"+ nonExistingId);
+
+        ObjectWriter writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE,false)
+                .writer();
+        String result = writer.writeValueAsString(error);
+
+        MvcResult response = mockMvc.perform(get("/users/{userId}/followers/list", nonExistingId))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+
+        assertEquals(result,response.getResponse().getContentAsString());
+    }
+
 
 }
