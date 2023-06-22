416e8132181cff77d3ba520ffd8b0664f3f7e140
Ezequiel Safdie <ezequiel.safdie@mercadolibre.com>
2023-06-22 16:28:59 -0300

    Cambiado el nombre del package unitary -> unit

--- 6. sprint II/be_java_hisp_w21_g1/src/test/java/com/example/be_java_hisp_w21_g1/unit/UserServiceTest.java
+++ 6. sprint II/be_java_hisp_w21_g1/src/test/java/com/example/be_java_hisp_w21_g1/unit/UserServiceTest.java
@@ -1,4 +1,4 @@
-package com.example.be_java_hisp_w21_g1.unitary;
+package com.example.be_java_hisp_w21_g1.unit;
 
 import com.example.be_java_hisp_w21_g1.DTO.Response.*;
 import com.example.be_java_hisp_w21_g1.DTO.Response.FollowUserDTO;
@@ -13,7 +13,6 @@ import com.example.be_java_hisp_w21_g1.Model.Product;
 
 
 import com.example.be_java_hisp_w21_g1.Service.UserService;
-import net.bytebuddy.dynamic.DynamicType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
