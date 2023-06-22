3623fe2a91dda2059d899d692fc602770fe19141
Charlie Schiaffino <carlos.schiaffino@mercadolibre.cl>
2023-06-22 12:34:25 -0400

    (T-0007) Implementación final de tests de servicios getFollowersCount con sus respectivas excepciones.

--- be_java_hisp_w21_g04/src/test/java/com/sprint/be_java_hisp_w21_g04/unitTest/service/UserServiceImplTest.java
+++ be_java_hisp_w21_g04/src/test/java/com/sprint/be_java_hisp_w21_g04/unitTest/service/UserServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.sprint.be_java_hisp_w21_g04.unitTest.service;
 import com.sprint.be_java_hisp_w21_g04.dto.response.FollowedResponseDto;
 import com.sprint.be_java_hisp_w21_g04.dto.response.ResponseDto;
+import com.sprint.be_java_hisp_w21_g04.dto.response.UserFollowersCountDto;
 import com.sprint.be_java_hisp_w21_g04.dto.response.UserResponseDto;
 import com.sprint.be_java_hisp_w21_g04.entity.User;
 import com.sprint.be_java_hisp_w21_g04.exception.*;
@@ -39,6 +40,7 @@ public class UserServiceImplTest {
         when(userRepository.findUserById(userIdToFollow)).thenReturn(userToFollow);
         // Act
         ResponseDto responseDto = userService.followUser(userId, userIdToFollow);
+
         // Assert
         assertNotNull(responseDto);
         assertEquals("Has seguido a MikeJohnson", responseDto.getMessage());
@@ -149,5 +151,40 @@ public class UserServiceImplTest {
         assertThrows(UserUnfollowNotAllowedException.class, () -> userService.unfollowUser(userId, userIdToUnfollow));
         assertFalse(user.getFollowed().contains(userIdToUnfollow));
         assertTrue(user.getFollowed().isEmpty());
+    }
+
+    @Test
+    @DisplayName("T0007 - Caso ideal donde se obtiene la cantidad correcta de followers. (US-0002)")
+    void getFollowersCount(){
+        // Arrange
+        int userOneId = 1;
+        int userTwoId = 2;
+        int userThreeId = 3;
+        UserFollowersCountDto expectedFollowersCount = new UserFollowersCountDto();
+        expectedFollowersCount.setUserId(userThreeId);
+        expectedFollowersCount.setUserName("Jackie Chan");
+        expectedFollowersCount.setFollowersCount(2);
+        List<Integer> followedUsers = new ArrayList<>(List.of(userThreeId));
+        List<Integer> followersUsers = new ArrayList<>(List.of(userOneId, userTwoId));
+        User user1 = new User(userOneId,"DavidWilson",new ArrayList<>(), followedUsers);
+        User user2 = new User(userTwoId,"MikeJohnson", new ArrayList<>(), followedUsers);
+        // User 3 is followed by user 1 and user 2
+        User user3 = new User(userThreeId,"Jackie Chan", followersUsers, new ArrayList<>());
+        when(userRepository.findUserById(userThreeId)).thenReturn(user3);
+        // Act
+        UserFollowersCountDto responseDto = userService.getFollowersCount(userThreeId);
+        // Assert
+        assertNotNull(responseDto);
+        assertEquals(expectedFollowersCount, responseDto);
+    }
+
+    @Test
+    @DisplayName("T0007 - Caso cuando usuario no existe (US-0002)")
+    void getFollowersCountWhenUserNotExist(){
+        // Arrange
+        int userId = 1;
+        when(userRepository.findUserById(userId)).thenReturn(null);
+        // Act & Assert
+        assertThrows(UserNotFoundException.class, () -> userService.getFollowersCount(userId));
     }
 }
