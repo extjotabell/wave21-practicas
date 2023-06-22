78109a84769fbe2a6efcdd52a7c1e95965d054a9
Ezequiel Safdie <ezequiel.safdie@mercadolibre.com>
2023-06-22 17:14:00 -0300

    Agregado test de integracion para la creacion del post y la validacion de los parametros

--- 6. sprint II/be_java_hisp_w21_g1_safdie/src/test/java/com/example/be_java_hisp_w21_g1/integration/ControllerTest.java
+++ 6. sprint II/be_java_hisp_w21_g1_safdie/src/test/java/com/example/be_java_hisp_w21_g1/integration/ControllerTest.java
@@ -1,9 +1,14 @@
 package com.example.be_java_hisp_w21_g1.integration;
 
+import com.example.be_java_hisp_w21_g1.DTO.Request.PostProductDTO;
+import com.example.be_java_hisp_w21_g1.DTO.Response.ProductDTO;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,9 +16,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -113,4 +123,57 @@ public class ControllerTest {
                 .andExpect(jsonPath("$.message").value("No se encontro el usuario con el ID100"));
     }
 
+    // CREATE POST
+    @Test
+    @DisplayName("Verificar que se pueda crear un post")
+    void createPostTestHappy() throws Exception {
+        String expected = "{\"message\":\"Accion realizada con exito.\",\"status\":200}";
+        ProductDTO product = new ProductDTO(1, "Computadora Gamer", "Electronica", "Asus", "Negro", null);
+        PostProductDTO post = new PostProductDTO(1, LocalDate.now(), product, 100, 1500.00);
+
+
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/products/post")
+                .contentType("application/json")
+                .content(writer.writeValueAsString(post)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$").value("Se ha creado el post!"));
+    }
+
+    @Test
+    @DisplayName("Verificar que solo acepta daros validos")
+    void createPostTestUnhappy() throws Exception{
+        String[] expectedStrings = {"Price cannot be higher than 10.000.000",
+                "Color can't contain special characters", "Category can't be empty",
+                "The date cannot be null", "Product_id can't be less than 0",
+                "Brand name can't contain special characters",
+                "Type can't contain special characters or be empty",
+                "Notes can't contain special characters", "User_id can't be less than 0",
+                "Product name can't contain special characters or be empty"};
+
+        ProductDTO product = new ProductDTO(-1, "Computadora & Gamer", "Electronica &", "Asus $", "Negro $", "notas $");
+        PostProductDTO post = new PostProductDTO(-1, null, product, null, 100000000.00);
+
+
+        MvcResult result = this.mockMvc.perform(MockMvcRequestBuilders.post("/products/post")
+                        .contentType("application/json")
+                        .content(writer.writeValueAsString(post)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("The following errors were found: "))
+                .andExpect(jsonPath("$.allErrors").isArray())
+                .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(response);
+        JsonNode allErrorsNode = jsonNode.get("allErrors");
+        List<String> errorList = objectMapper.readValue(allErrorsNode.toString(), new TypeReference<List<String>>() {});
+
+        Assertions.assertTrue(errorList.containsAll(Arrays.asList(expectedStrings)));
+
+    }
 }
