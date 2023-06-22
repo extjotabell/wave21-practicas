e06b7af1387d8994a0d6eb69e7b163c20d2df5ac
hehernandezMELI <helmer.hernandez@mercadolibre.com.co>
2023-06-22 11:25:12 -0500

    Desarrollando test de excepciones para el metodo SellerFollowedListPostResponseDto en el servicio de PostServiceImpl

--- be_java_hisp_w21_g04/src/test/java/com/sprint/be_java_hisp_w21_g04/unitTest/service/PostServiceImplTest.java
+++ be_java_hisp_w21_g04/src/test/java/com/sprint/be_java_hisp_w21_g04/unitTest/service/PostServiceImplTest.java
@@ -5,6 +5,9 @@ import com.sprint.be_java_hisp_w21_g04.dto.response.SellerFollowedListPostRespon
 import com.sprint.be_java_hisp_w21_g04.entity.Post;
 import com.sprint.be_java_hisp_w21_g04.entity.Product;
 import com.sprint.be_java_hisp_w21_g04.entity.User;
+import com.sprint.be_java_hisp_w21_g04.exception.EmptySellerFollowedList;
+import com.sprint.be_java_hisp_w21_g04.exception.IllegalDataException;
+import com.sprint.be_java_hisp_w21_g04.exception.UserNotFoundException;
 import com.sprint.be_java_hisp_w21_g04.repository.post.PostRepositoryImpl;
 import com.sprint.be_java_hisp_w21_g04.repository.user.IUserRepository;
 import com.sprint.be_java_hisp_w21_g04.service.post.PostServiceImpl;
@@ -17,9 +20,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,19 +48,19 @@ public class PostServiceImplTest {
 
         List<Post> mockResult = new ArrayList<>();
         Product product1 = new Product(1, "Camiseta", "T Shirt", "Nike", "Red", "");
-        mockResult.add(new Post(userId, LocalDate.of(2023, 6, 15), product1, 10, 100, true, 10));
+        mockResult.add(new Post(userId, LocalDate.now().minusDays(10), product1, 10, 100, true, 10));
         Product product2 = new Product(1, "Short", "Short", "Adidas", "Brown", "");
-        mockResult.add(new Post(userId, LocalDate.of(2023, 6, 18), product2, 6, 160, true, 10));
+        mockResult.add(new Post(userId, LocalDate.now().minusDays(5), product2, 6, 160, true, 10));
         Product product3 = new Product(1, "Camiseta", "T Shirt", "Adidas", "White", "");
-        mockResult.add(new Post(1, LocalDate.of(2023, 6, 20), product3, 4, 120, true, 10));
+        mockResult.add(new Post(1, LocalDate.now(), product3, 4, 120, true, 10));
 
         List<PostResponseDto> expectedPosts = new ArrayList<>();
         Product product4 = new Product(1, "Camiseta", "T Shirt", "Nike", "Red", "");
-        expectedPosts.add(new PostResponseDto(userId, LocalDate.of(2023, 6, 15), product4, 10, 100));
+        expectedPosts.add(new PostResponseDto(userId, LocalDate.now().minusDays(10), product4, 10, 100));
         Product product5 = new Product(1, "Short", "Short", "Adidas", "Brown", "");
-        expectedPosts.add(new PostResponseDto(userId, LocalDate.of(2023, 6, 18), product5, 6, 160));
+        expectedPosts.add(new PostResponseDto(userId, LocalDate.now().minusDays(5), product5, 6, 160));
         Product product6 = new Product(1, "Camiseta", "T Shirt", "Adidas", "White", "");
-        expectedPosts.add(new PostResponseDto(1, LocalDate.of(2023, 6, 20), product6, 4, 120));
+        expectedPosts.add(new PostResponseDto(1, LocalDate.now(), product6, 4, 120));
         SellerFollowedListPostResponseDto expectedResult = new SellerFollowedListPostResponseDto(userId, expectedPosts);
 
         List<Integer> followers = Arrays.asList(1, 2, 3, 5);
@@ -69,6 +75,8 @@ public class PostServiceImplTest {
         SellerFollowedListPostResponseDto result = postService.sellerFollowedListPosts(userId, order);
 
         //Assert
+        assertEquals(3, result.getPosts().size());
+        assertTrue(result.getPosts().get(0).getDate().isBefore(result.getPosts().get(1).getDate()));
         assertEquals(expectedResult, result);
     }
     @Test
@@ -79,19 +87,19 @@ public class PostServiceImplTest {
 
         List<Post> mockResult = new ArrayList<>();
         Product product1 = new Product(1, "Camiseta", "T Shirt", "Nike", "Red", "");
-        mockResult.add(new Post(userId, LocalDate.of(2023, 6, 20), product1, 10, 100, true, 10));
+        mockResult.add(new Post(userId, LocalDate.now(), product1, 10, 100, true, 10));
         Product product2 = new Product(1, "Short", "Short", "Adidas", "Brown", "");
-        mockResult.add(new Post(userId, LocalDate.of(2023, 6, 18), product2, 6, 160, true, 10));
+        mockResult.add(new Post(userId, LocalDate.now().minusDays(5), product2, 6, 160, true, 10));
         Product product3 = new Product(1, "Camiseta", "T Shirt", "Adidas", "White", "");
-        mockResult.add(new Post(1, LocalDate.of(2023, 6, 15), product3, 4, 120, true, 10));
+        mockResult.add(new Post(1, LocalDate.now().minusDays(10), product3, 4, 120, true, 10));
 
         List<PostResponseDto> expectedPosts = new ArrayList<>();
         Product product4 = new Product(1, "Camiseta", "T Shirt", "Nike", "Red", "");
-        expectedPosts.add(new PostResponseDto(userId, LocalDate.of(2023, 6, 20), product4, 10, 100));
+        expectedPosts.add(new PostResponseDto(userId, LocalDate.now(), product4, 10, 100));
         Product product5 = new Product(1, "Short", "Short", "Adidas", "Brown", "");
-        expectedPosts.add(new PostResponseDto(userId, LocalDate.of(2023, 6, 18), product5, 6, 160));
+        expectedPosts.add(new PostResponseDto(userId, LocalDate.now().minusDays(5), product5, 6, 160));
         Product product6 = new Product(1, "Camiseta", "T Shirt", "Adidas", "White", "");
-        expectedPosts.add(new PostResponseDto(1, LocalDate.of(2023, 6, 15), product6, 4, 120));
+        expectedPosts.add(new PostResponseDto(1, LocalDate.now().minusDays(10), product6, 4, 120));
         SellerFollowedListPostResponseDto expectedResult = new SellerFollowedListPostResponseDto(userId, expectedPosts);
 
         List<Integer> followers = Arrays.asList(1, 2, 3, 5);
@@ -106,7 +114,106 @@ public class PostServiceImplTest {
         SellerFollowedListPostResponseDto result = postService.sellerFollowedListPosts(userId, order);
 
         //Assert
+        assertEquals(3, result.getPosts().size());
+        assertTrue(result.getPosts().get(0).getDate().isAfter(result.getPosts().get(1).getDate()));
         assertEquals(expectedResult, result);
+    }
+    @Test
+    public void testPostsGetAll(){
+        //Arrange
+        List<Post> mockResult = new ArrayList<>();
+        Product product1 = new Product(1, "Camiseta", "T Shirt", "Nike", "Red", "");
+        mockResult.add(new Post(1, LocalDate.now(), product1, 10, 100, true, 10));
+        Product product2 = new Product(1, "Short", "Short", "Adidas", "Brown", "");
+        mockResult.add(new Post(2, LocalDate.now().minusWeeks(3), product2, 6, 160, true, 10));
+        Product product3 = new Product(1, "Camiseta", "T Shirt", "Adidas", "White", "");
+        mockResult.add(new Post(3, LocalDate.now().minusDays(20), product3, 4, 120, true, 10));
+
+        List<PostResponseDto> expectedPosts = new ArrayList<>();
+        Product product4 = new Product(1, "Camiseta", "T Shirt", "Nike", "Red", "");
+        expectedPosts.add(new PostResponseDto(1, LocalDate.now(), product4, 10, 100));
+        Product product5 = new Product(1, "Short", "Short", "Adidas", "Brown", "");
+        expectedPosts.add(new PostResponseDto(2, LocalDate.now().minusWeeks(3), product5, 6, 160));
+        Product product6 = new Product(1, "Camiseta", "T Shirt", "Adidas", "White", "");
+        expectedPosts.add(new PostResponseDto(3, LocalDate.now().minusDays(20), product6, 4, 120));
+
+
+        when(postRepository.getAll()).thenReturn(mockResult);
+
+
+        //Act
+        List<PostResponseDto> result = postService.getAll();
+
+        //Assert
+        assertEquals(3, result.size());
+        assertEquals(expectedPosts, result);
+    }
+
+    @Test
+    public void testUserNotFoundException(){
+        //arrange
+        when(userRepository.getById(anyInt())).thenReturn(null);
+
+        //Act and assert
+        assertThrows(UserNotFoundException.class, ()-> postService.sellerFollowedListPosts(1, "date_asc"));
+    }
+
+    @Test
+    public void testEmptySellerFollowedList(){
+        //Arrange
+        int userId = 1;
+        String order = "date_desc";
+
+        List<Integer> followers = Arrays.asList(1, 2, 3, 5);
+        List<Integer> followed = Arrays.asList(1, 2, 3, 5);
+        User user = new User(1, "Pedro", followers, followed);
+
+        when(userRepository.getById(userId)).thenReturn(user);
+        when(postRepository.getSellerFollowed(userId)).thenReturn(Collections.emptyList());
+
+        //Act and Assert
+        assertThrows(EmptySellerFollowedList.class, () -> postService.sellerFollowedListPosts(userId, order));
+    }
+
+    @Test
+    public void testEmptySellerFollowedListWhenNoRecentPosts(){
+        //Arrange
+        int userId = 1;
+        String order = "date_desc";
+        User user = mock(User.class);
+        Post oldPost = mock(Post.class);
+
+        when(oldPost.getDate()).thenReturn(LocalDate.now().minusWeeks(3));
+        when(userRepository.getById(anyInt())).thenReturn(user);
+        when(postRepository.getSellerFollowed(anyInt())).thenReturn(List.of(oldPost));
+
+        //Act and Assert
+        assertThrows(EmptySellerFollowedList.class, () -> postService.sellerFollowedListPosts(userId, order));
+    }
+
+    @Test
+    public void testIllegalDataException(){
+        //Arrange
+        int userId = 1;
+        String order = "XXXXXXX";
+
+        //Act and Assert
+        assertThrows(IllegalDataException.class, ()-> postService.sellerFollowedListPosts(userId, order));
+    }
+    @Test
+    public void testNoExceptions(){
+        //Arrange
+        int userId = 1;
+        String order = "date_desc";
+        User user = mock(User.class);
+        Post oldPost = mock(Post.class);
+
+        when(oldPost.getDate()).thenReturn(LocalDate.now());
+        when(userRepository.getById(anyInt())).thenReturn(user);
+        when(postRepository.getSellerFollowed(anyInt())).thenReturn(List.of(oldPost));
+
+        //Act and Assert
+        assertDoesNotThrow(()-> postService.sellerFollowedListPosts(userId, order));
     }
 
 }
