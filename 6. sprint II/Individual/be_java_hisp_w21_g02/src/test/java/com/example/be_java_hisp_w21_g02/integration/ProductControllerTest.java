0dff3cf30f0c70c7da524d8a54e0f1eee4d1fa8a
Gonza Marquez <gonzalo.marquez@mercadolibre.com>
2023-06-23 14:25:29 -0300

    Changes of file from different directories.

--- 6. sprint II/Individual/be_java_hisp_w21_g02/src/test/java/com/example/be_java_hisp_w21_g02/integration/ProductControllerTest.java
+++ 6. sprint II/Individual/be_java_hisp_w21_g02/src/test/java/com/example/be_java_hisp_w21_g02/integration/ProductControllerTest.java
@@ -1,4 +1,135 @@
 package com.example.be_java_hisp_w21_g02.integration;
 
+import com.example.be_java_hisp_w21_g02.dto.ProductDTO;
+import com.example.be_java_hisp_w21_g02.dto.request.PostRequestDTO;
+import com.example.be_java_hisp_w21_g02.dto.response.FollowedListDTO;
+import com.example.be_java_hisp_w21_g02.dto.response.UserPostResponseDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ProductControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectWriter writer;
+
+    ProductDTO productRequest;
+    PostRequestDTO postRequest;
+
+    @BeforeEach
+    void setUp() {
+        writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .registerModule(new JavaTimeModule())
+                .setDateFormat(new SimpleDateFormat("dd-MM-yyyy"))
+                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                .writer();
+        productRequest = new ProductDTO(1, "HyperX Allow FPS", "HyperX", "Teclado", "Iron Black","New keyboard for gaming");
+        postRequest = new PostRequestDTO(1, LocalDate.now(), productRequest, BigInteger.valueOf(8000), 35000.25);
+    }
+
+    @Test
+    @DisplayName("Integration Test US05 01 - Creation of Post is Ok")
+    void createProductOk() throws Exception {
+        // Arrange
+        String payload = writer.writeValueAsString(postRequest);
+
+        // Act & Assert
+        mockMvc.perform(post("/products/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Integration Test US05 02 - Creation of Post fails with the Post's Date")
+    void createProductFailsPostDate() throws Exception {
+        // Arrange
+        postRequest.setDate(LocalDate.now().plusDays(5));
+        String payload = writer.writeValueAsString(postRequest);
+
+        // Act & Assert
+        mockMvc.perform(post("/products/post")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(payload))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("description").value("Invalid date, It should be as Past or present date"));
+    }
+
+    @Test
+    @DisplayName("Integration Test US05 03 - Creation of Post fails with the Post's Price")
+    void createProductFailsPostPrice() throws Exception {
+        // Arrange
+        postRequest.setPrice(null);
+        String payload = writer.writeValueAsString(postRequest);
+
+        // Act & Assert
+        mockMvc.perform(post("/products/post")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(payload))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("description").value("Price is required"));
+    }
+
+    @Test
+    @DisplayName("Integration Test US05 04 - Creation of Post fails with the Post's Product")
+    void createProductFailsPostProduct() throws Exception {
+        // Arrange
+        postRequest.getProduct().setType("!nv@l!dN@m#");
+        String payload = writer.writeValueAsString(postRequest);
+
+        // Act & Assert
+        mockMvc.perform(post("/products/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(payload))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("description").value("The type field cannot have special characters"));
+    }
+
+    @Test
+    @DisplayName("Integration Test US06 01 - Cannot get posts list")
+    void getPostListUserNotExists() throws Exception {
+        // Arrange
+        int userId = 1;
+
+        UserPostResponseDTO expectedResult = new UserPostResponseDTO(1, new ArrayList<>());
+        String expectedJson = writer.writeValueAsString(expectedResult);
+        ResultMatcher expected = content().json(expectedJson);
+        // Act & Assert
+        mockMvc.perform(get("/products/followed/{userId}/list", userId))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(expected);
+    }
 }
--- 6. sprint II/Individual/be_java_hisp_w21_g02/src/test/java/com/example/be_java_hisp_w21_g02/integration/UserControllerTest.java
+++ 6. sprint II/Individual/be_java_hisp_w21_g02/src/test/java/com/example/be_java_hisp_w21_g02/integration/UserControllerTest.java
@@ -1,4 +1,144 @@
 package com.example.be_java_hisp_w21_g02.integration;
 
+import com.example.be_java_hisp_w21_g02.dto.response.FollowedListDTO;
+import com.example.be_java_hisp_w21_g02.dto.response.FollowersListDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Integration Test US01 01 - User not exists")
+    void userFollowSellerUserNotExists() throws Exception{
+        int userIdNotExist = 99;
+        int userIdExists = 2;
+        mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}", userIdNotExist, userIdExists))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("error_message").value("This ID doesn't correspond to any user"));
+    }
+
+    @Test
+    @DisplayName("Integration Test US01 02 - User to follow not seller")
+    void userFollowSellerNotSeller() throws Exception{
+        int userIdExists = 1;
+        int userNotSeller = 2;
+        mockMvc.perform(post("/users/{userId}/follow/{userIdToFollow}", userIdExists, userNotSeller))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("error_message").value("This user is not a seller"));
+    }
+
+    @Test
+    @DisplayName("Integration Test US02 01 - User not seller")
+    void getFollowersCountFail() throws Exception{
+        int userNotSeller = 2;
+        mockMvc.perform(get("/users/{userId}/followers/count", userNotSeller))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("error_message").value("This user is not a seller"));
+    }
+
+    @Test
+    @DisplayName("Integration Test US07 01 - User not exist on unfollow")
+    void unfollowUserNotExistFail() throws Exception {
+        int userIdNotExists = 999;
+        int userNotSeller = 2;
+        mockMvc.perform(post("/users/{userId}/unfollow/{userIdToUnfollow}", userIdNotExists, userNotSeller)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("error_message").value("This ID doesn't correspond to any user"));
+    }
+
+    @Test
+    @DisplayName("Integration Test US07 01 - User to unfollow not seller")
+    void unfollowUserNotSellerFail() throws Exception {
+        int userIdExists = 1;
+        int userNotSeller = 2;
+        mockMvc.perform(post("/users/{userId}/unfollow/{userIdToUnfollow}", userIdExists, userNotSeller)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("error_message").value("This user is not a seller"));
+    }
+
+    @Test
+    @DisplayName("Integration Test US03 01 - Get user followed list but user doesn't exists")
+    void userGetFollowedListUserNotExists() throws Exception {
+        int userIdNotExists = 999;
+        mockMvc.perform(get("/users/{userId}/followed/list", userIdNotExists))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("error_message").value("We couldn't find a user with the mentioned ID"));
+    }
+
+    @Test
+    @DisplayName("Integration Test US03 02 - Get user followed list")
+    void userGetFollowedListUser() throws Exception {
+        int userId = 1;
+
+        ObjectWriter writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .registerModule(new JavaTimeModule())
+                .setDateFormat(new SimpleDateFormat("dd-MM-yyyy"))
+                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                .writer();
+
+        FollowedListDTO expectedResult = new FollowedListDTO(1, "JavierRydel", new ArrayList<>());
+
+        String expectedJson = writer.writeValueAsString(expectedResult);
+
+        ResultMatcher expected = content().json(expectedJson);
+
+        mockMvc.perform(get("/users/{userId}/followed/list", userId))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(expected);
+    }
+
+    @Test
+    @DisplayName("Integration Test US03 02 - Get user followers list but is not a seller")
+    void userGetFollowersListUserNotSeller() throws Exception {
+        int userId = 1;
+
+        mockMvc.perform(get("/users/{userId}/followers/list", userId))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("error_message").value("This user is not a seller"));
+
+
+    }
 }
