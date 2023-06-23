2080b76610a477324fe6a88b2b4742cb31c1b9ef
marielebarri <marielena.barrios@mercadolibre.com.co>
2023-06-23 09:31:37 -0500

    Eliminando Carpeta individual

--- 6. sprint II/INDIVIDUAL/be_java_hisp_w21_g04_Barrios/src/main/java/com/sprint/be_java_hisp_w21_g04/exception/UserFollowNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.be_java_hisp_w21_g04.exception;
-
-public class UserFollowNotAllowedException extends RuntimeException {
-    public UserFollowNotAllowedException(String message) {
-        super(message);
-    }
-}
--- 6. sprint II/INDIVIDUAL/be_java_hisp_w21_g04_Barrios/src/test/java/com/sprint/be_java_hisp_w21_g04/controller/integration/UserControllerTest.java
+++
@@ -1,4 +0,0 @@
-package com.sprint.be_java_hisp_w21_g04.controller.integration;
-
-public class UserControllerTest {
-}
