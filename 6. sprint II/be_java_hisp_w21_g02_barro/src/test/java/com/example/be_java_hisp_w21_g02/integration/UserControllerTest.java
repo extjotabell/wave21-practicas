d51d4d62744426815b583f0c52e5a8409c0e51a4
gbarro <gaston.barro@mercadolibre.com>
2023-06-23 00:39:29 -0300

    Added Integration Test for US02, User not seller

--- 6. sprint II/be_java_hisp_w21_g02_barro/src/test/java/com/example/be_java_hisp_w21_g02/integration/UserControllerTest.java
+++ 6. sprint II/be_java_hisp_w21_g02_barro/src/test/java/com/example/be_java_hisp_w21_g02/integration/UserControllerTest.java
@@ -1,4 +1,36 @@
 package com.example.be_java_hisp_w21_g02.integration;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    @DisplayName("Integration Test US02 T-0010 01 - Fail User not seller")
+    void getFollowersCountFail() throws Exception{
+        MvcResult mvcResult = mockMvc.perform(get("/users/2/followers/count"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("error_message").value("This user is not a seller"))
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
 }
