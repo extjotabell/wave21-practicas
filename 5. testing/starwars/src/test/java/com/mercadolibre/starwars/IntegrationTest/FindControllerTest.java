0db6baa9cde2adbd514e29b796b79c506be1c3c2
ornellaquarracino <ornella.quarracino@mercadolibre.com>
2023-06-26 15:20:39 -0300

    subiendo ejercicio de Testing de Starwars

---
+++ 5. testing/starwars/src/test/java/com/mercadolibre/starwars/IntegrationTest/FindControllerTest.java
@@ -0,0 +1,50 @@
+package com.mercadolibre.starwars.IntegrationTest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.mercadolibre.starwars.dto.CharacterDTO;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FindControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void findTest() throws Exception{
+        List<CharacterDTO> expectedResult = List.of(
+                new CharacterDTO("Luke Skywalker", "blond", "fair", "blue", "19BBY", "male", "Tatooine", "Human", 172, 77),
+                new CharacterDTO("Anakin Skywalker", "blond", "fair", "blue", "41.9BBY", "male", "Tatooine", "Human", 188, 84),
+                new CharacterDTO("Shmi Skywalker", "black", "fair", "brown", "72BBY", "female", "Tatooine", "Human", 163, null)
+        );
+        ObjectWriter writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE,false)
+                .writer();
+
+        String responseJsonAsString = writer.writeValueAsString(expectedResult);
+
+        MvcResult mvcResult = mockMvc.perform(get("/{query}", "Skywalker"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+
+        assertEquals(responseJsonAsString,mvcResult.getResponse().getContentAsString());
+    }
+}
