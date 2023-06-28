5565f8578e0a94a7ae3b73c93f1c3f2214eef292
ornellaquarracino <ornella.quarracino@mercadolibre.com>
2023-06-28 15:02:22 -0300

    Subiendo ej individual de BD relacional - Movies

--- 7. bd relacionales/Movies/consultas_movies.sql
+++ 7. bd relacionales/Movies/consultas_movies.sql
@@ -28,3 +28,25 @@ select title from movies where rating >3
 and awards >1 
 and release_date between "1988-01-01" and "2009-12-31" 
 order by rating;
+
+-- Parte 2 - PG
+select title, g.name from series s
+inner join genres g on s.genre_id = g.id;
+
+select e.title, a.first_name, a.last_name from episodes e
+inner join actor_episode ae on e.id = ae.episode_id
+inner join actors a on ae.actor_id = a.id;
+
+select series.title, count(serie_id) totalSeasons from series 
+inner join seasons on seasons.serie_id = series.id
+group by series.id;
+
+select g.name, count(m.id) from genres g
+inner join movies m on g.id = m.genre_id
+group by genre_id
+having count(m.id) >= 3;
+
+select distinct a.first_name, a.last_name from actors a
+inner join actor_movie am on am.actor_id = a.id
+inner join movies m on am.movie_id = m.id
+where m.title like "La guerra de las galaxias%";
