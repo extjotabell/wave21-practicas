e0e08a01a8b095b39ae285e0cd5992b79d19d4ea
ddelavegaML <demian.delavega@mercadolibre.com>
2023-07-06 15:39:15 -0300

    Inicializacion de proyecto JoyeriaApp

---
+++ 9.  implementacion de bases de datos/joyeriaApp/src/main/java/com/example/joyeriaApp/JoyeriaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.joyeriaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyeriaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoyeriaAppApplication.class, args);
+	}
+
+}
