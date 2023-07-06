327fe5a0611cdae9876a1a37b9a991bf51d4ca1f
José Luis <joseluis.gonzalez@mercadolibre.cl>
2023-07-06 12:04:33 -0400

    Adding miniserie project EN-VIVO jpa config

---
+++ 9.  implementacion de bases de datos/mini-serie EN-VIVO/src/main/java/com/mini/serie/miniserie/model/MiniSerie.java
@@ -0,0 +1,21 @@
+package com.mini.serie.miniserie.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter @Getter
+@Entity
+@Table(name = "miniserie")
+public class MiniSerie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double rating;
+
+    @Column(name = "amount_of_awards")
+    private Integer amountOfAwards;
+}
