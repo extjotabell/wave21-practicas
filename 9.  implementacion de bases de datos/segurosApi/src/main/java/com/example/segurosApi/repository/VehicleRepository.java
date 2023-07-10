c438e8eba00a1ee8e575b52a5ef8c44810ac0538
ddelavegaML <demian.delavega@mercadolibre.com>
2023-07-10 16:19:52 -0300

    Queries HQL para los vehiculos almacenados en DB.

--- 9.  implementacion de bases de datos/segurosApi/src/main/java/com/example/segurosApi/repository/VehicleRepository.java
+++ 9.  implementacion de bases de datos/segurosApi/src/main/java/com/example/segurosApi/repository/VehicleRepository.java
@@ -10,4 +10,23 @@ import java.util.List;
 
 public interface VehicleRepository extends JpaRepository<Vehiculo, Long> {
 
+    @Query("SELECT v.patente FROM Vehiculo v")
+    List<String> findAllPatents();
+
+    //Listar la patente y la marca de todos los vehículos ordenados por año de fabricación.
+    @Query("SELECT v.patente, v.modelo FROM Vehiculo v ORDER BY v.fabricacion")
+    List<Vehiculo> findAllVehicleOrdenByManufacturing();
+
+    //Listar la patente de todos los vehículos que tengan más de cuatro ruedas y hayan sido fabricados en el corriente año.
+    @Query("SELECT v.patente FROM Vehiculo v WHERE v.cantRuedas > 4 AND YEAR(v.fabricacion) = YEAR(current_date)")
+    List<String> findAllPatentsByCantRuedasAndCurrentYear();
+
+    //Listar la matrícula, marca y modelo de todos los vehículos que hayan tenido un siniestro con pérdida mayor de 10000 pesos.
+    @Query("SELECT v FROM Vehiculo v " +
+            "WHERE v.id IN (SELECT s.vehiculo.id FROM v.siniestros s WHERE s.perdidaEconomica > 10000 ) ")
+    List<Vehiculo> findAllPatentAndBrandAndModel();
+
+    //Listar la matrícula, marca y modelo de todos los vehículos que hayan tenido un siniestro con pérdida mayor de 10000 pesos
+    // y mostrar a cuánto ascendió la pérdida total de todos ellos.
+
 }
