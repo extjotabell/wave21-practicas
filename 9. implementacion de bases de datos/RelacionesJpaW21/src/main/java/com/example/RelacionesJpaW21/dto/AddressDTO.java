2ee8ab540c8a8bf6035d3ae94eeb3dcc110e3847
nhdebellis <nahuel.bellis@mercadolibre.com>
2023-07-10 10:52:25 -0300

    reestructura de archivos

--- 9. implementacion de bases de datos/RelacionesJpaW21/src/main/java/com/example/RelacionesJpaW21/dto/AddressDTO.java
+++ 9. implementacion de bases de datos/RelacionesJpaW21/src/main/java/com/example/RelacionesJpaW21/dto/AddressDTO.java
@@ -1,7 +1,13 @@
 package com.example.RelacionesJpaW21.dto;
 
-public class AddressDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+public class AddressDTO {
     private String street;
     private int number;
 }
