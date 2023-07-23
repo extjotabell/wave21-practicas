144cc2ec55c5dae84ba05291e02afa6c67546f72
mercedessabaj <mercedes.msabaj@mercadolibre.com>
2023-07-23 18:33:17 -0300

    Agrego ejercicios SQL consultas avanzadas VIVO empresa_db

---
+++ 7. bd relacionales/consultas_SQL_avanzadas - VIVO/consultas_empresa_db.sql
@@ -0,0 +1,43 @@
+-- Seleccionar el nombre, el puesto y la localidad de los departamentos donde trabajan los vendedores.
+SELECT dep.nombre_depto, emp.puesto, dep.localidad
+FROM departamento dep, empleado emp
+WHERE emp.depto_nro = dep.depto_nro AND emp.puesto = "vendedor";
+
+-- Visualizar los departamentos con más de cinco empleados.
+SELECT dep.*
+FROM departamento dep, empleado emp
+WHERE dep.depto_nro = emp.depto_nro
+GROUP BY depto_nro
+HAVING COUNT(*) > 5;
+
+-- Mostrar el nombre, salario y nombre del departamento de los empleados que tengan el mismo puesto que ‘Mito Barchuk’.
+SELECT emp.nombre, emp.salario, dep.nombre_depto
+FROM empleado emp, departamento dep
+WHERE emp.puesto IN(
+	SELECT puesto
+    FROM empleado
+    WHERE nombre = "Mito" AND apellido = "Barchuk")
+AND emp.depto_nro = dep.depto_nro;
+
+-- Mostrar los datos de los empleados que trabajan en el departamento de contabilidad, ordenados por nombre.
+SELECT empleado.*
+FROM empleado, departamento
+WHERE empleado.depto_nro IN (
+	SELECT depto_nro
+    FROM departamento
+    WHERE departamento.nombre_depto = "Contabilidad")
+AND empleado.depto_nro = departamento.depto_nro
+ORDER BY empleado.nombre DESC, empleado.apellido DESC;
+
+-- Mostrar el nombre del empleado que tiene el salario más bajo.
+SELECT nombre 
+FROM empleado
+ORDER BY salario
+LIMIT 1;
+
+-- Mostrar los datos del empleado que tiene el salario más alto en el departamento de ‘Ventas’.
+SELECT *
+FROM empleado, departamento
+WHERE departamento.depto_nro = empleado.depto_nro AND departamento.nombre_depto = "Ventas"
+ORDER BY empleado.salario DESC
+LIMIT 1;
